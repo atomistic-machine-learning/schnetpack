cdc1f8672a9042e1164f62d48e72996aa38526e9
Michael Gastegger <michael.gastegger@tu-berlin.de>
2018-09-14 10:07:20 +0200

    Updated examples

--- src/examples/qm9_wACSF.py
+++ src/examples/qm9_wACSF.py
@@ -8,7 +8,7 @@ import schnetpack.representation as rep
 from schnetpack.datasets import *
 
 # load qm9 dataset and download if necessary
-data = QM9("qm9/", properties=[QM9.U0], calculate_triples=True)
+data = QM9("qm9/", properties=[QM9.U0], collect_triples=True)
 
 # split in train and val
 train, val, test = data.create_splits(100000, 10000)
@@ -16,7 +16,7 @@ loader = spk.data.AtomsLoader(train, batch_size=100, num_workers=4)
 val_loader = spk.data.AtomsLoader(val)
 
 # create model
-reps = rep.BehlerSFBlock()
+reps = rep.SymmetryFunctions(22, 5)
 output = atm.ElementalAtomwise(reps.n_symfuncs)
 model = atm.AtomisticModel(reps, output)
 
@@ -25,9 +25,9 @@ trainable_params = filter(lambda p: p.requires_grad, model.parameters())
 
 # create trainer
 opt = Adam(trainable_params, lr=1e-4)
-loss = lambda b, p: F.mse_loss(p["y"], b[QM9.U0])
+loss = lambda b, p: F.mse_loss(p[0], b[QM9.U0])
 trainer = spk.train.Trainer("wacsf/", model, loss,
-                      opt, loader, val_loader)
+                            opt, loader, val_loader)
 
 # start training
 trainer.train(torch.device("cpu"))
