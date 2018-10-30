d8d752b8b46859c7c22696bcd525a893d46aab71
Kristof Schütt <kristof.schuett@tu-berlin.de>
2018-10-30 10:28:42 +0100

    Fix #22. Band gap property added to MP data

--- src/schnetpack/datasets/matproj.py
+++ src/schnetpack/datasets/matproj.py
@@ -46,7 +46,8 @@ class MaterialsProject(AtomsData):
             )
     )
 
-    def __init__(self, path, cutoff, apikey=None, download=True, subset=None, properties=[], collect_triples=False):
+    def __init__(self, path, cutoff, apikey=None, download=True, subset=None,
+                 properties=[], collect_triples=False):
         self.path = path
         self.cutoff = cutoff
         self.apikey = apikey
@@ -60,13 +61,16 @@ class MaterialsProject(AtomsData):
         if download and not os.path.exists(self.dbpath):
             self._download()
 
-        super(MaterialsProject, self).__init__(self.dbpath, subset, properties, environment_provider, collect_triples)
+        super(MaterialsProject, self).__init__(self.dbpath, subset, properties,
+                                               environment_provider,
+                                               collect_triples)
 
     def create_subset(self, idx):
         idx = np.array(idx)
         subidx = idx if self.subset is None else np.array(self.subset)[idx]
 
-        return MaterialsProject(self.path, self.cutoff, download=False, subset=subidx, properties=self.properties,
+        return MaterialsProject(self.path, self.cutoff, download=False,
+                                subset=subidx, properties=self.properties,
                                 collect_triples=self.collect_triples)
 
     def _download(self):
@@ -81,24 +85,34 @@ class MaterialsProject(AtomsData):
             from pymatgen.core import Structure
             import pymatgen as pmg
         except:
-            raise ImportError('In order to download Materials Project data, you have to install pymatgen')
+            raise ImportError(
+                'In order to download Materials Project data, you have to install pymatgen')
 
         with connect(self.dbpath) as con:
             with MPRester(self.apikey) as m:
                 for N in range(1, 9):
                     for nsites in range(0, 300, 30):
                         ns = {"$lt": nsites + 31, "$gt": nsites}
-                        query = m.query(criteria={'nelements': N, 'is_compatible': True, 'nsites': ns},
-                                        properties=['structure', 'energy_per_atom', 'formation_energy_per_atom',
-                                                    'total_magnetization',
-                                                    'material_id', 'warnings'])
+                        query = m.query(
+                            criteria={'nelements': N, 'is_compatible': True,
+                                      'nsites': ns},
+                            properties=['structure', 'energy_per_atom',
+                                        'formation_energy_per_atom',
+                                        'total_magnetization', 'band_gap',
+                                        'material_id', 'warnings'])
 
                         for k, q in enumerate(query):
-                            s = q['structure']  # .get_primitive_structure(tolerance=0.1)
+                            s = q['structure']
                             if type(s) is Structure:
                                 at = Atoms(numbers=s.atomic_numbers,
                                            positions=s.cart_coords,
                                            cell=s.lattice.matrix, pbc=True)
-                                con.write(at, data={MaterialsProject.EPerAtom: q['energy_per_atom'],
-                                                    MaterialsProject.EformationPerAtom: q['formation_energy_per_atom'],
-                                                    MaterialsProject.TotalMagnetization: q['total_magnetization']})
+                                con.write(at, data={
+                                    MaterialsProject.EPerAtom: q[
+                                        'energy_per_atom'],
+                                    MaterialsProject.EformationPerAtom: q[
+                                        'formation_energy_per_atom'],
+                                    MaterialsProject.TotalMagnetization: q[
+                                        'total_magnetization'],
+                                    MaterialsProject.BandGap: q[
+                                        'band_gap']})
