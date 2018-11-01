53c68491a8c0e27ce4d21e85e767cce6e3176e8d
stefaan <hessmann.stefaan@googlemail.com>
2018-11-01 18:21:19 +0100

    small fix in path

--- tests/test_example_scripts.py
+++ tests/test_example_scripts.py
@@ -1,3 +1,4 @@
+import os
 import pytest
 from torch.optim import Adam
 import shutil
@@ -11,7 +12,8 @@ from schnetpack.metrics import *
 
 @pytest.fixture
 def data():
-    return QM9("test_data/", properties=[QM9.U0])
+    print(os.path.dirname(os.path.realpath(__file__)))
+    return QM9(os.path.dirname(os.path.realpath(__file__)) + "/test_data/", properties=[QM9.U0])
 
 @pytest.fixture
 def data_splits(data):
