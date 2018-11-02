Quick fix in neighbor calculation

src/schnetpack/nn/base.py
@@ -142,9 +142,10 @@ class Aggregate(nn.Module):
         mean (bool): use average instead of sum pooling
     """
 
-    def __init__(self, axis, mean=False):
+    def __init__(self, axis, mean=False, keepdim=True):
         self.average = mean
         self.axis = axis
+        self.keepdim = keepdim
         super(Aggregate, self).__init__()
 
     def forward(self, input, mask=None):
@@ -162,7 +163,7 @@ class Aggregate(nn.Module):
 
         if self.average:
             if mask is not None:
-                N = torch.sum(mask, self.axis, keepdim=True)
+                N = torch.sum(mask, self.axis, keepdim=self.keepdim)
                 N = torch.max(N, other=torch.ones_like(N))
             else:
                 N = input.size(self.axis)

src/schnetpack/nn/neighbors.py
@@ -51,7 +51,10 @@ def atom_distances(positions, neighbors, cell=None, cell_offsets=None, return_di
         distances = tmp_distances
 
     if return_directions:
-        direction = dist_vec / distances[:, :, :, None]
+        tmp_distances = torch.ones_like(distances)
+        tmp_distances[neighbor_mask != 0] = distances[neighbor_mask != 0]
+
+        direction = dist_vec / tmp_distances[:, :, :, None]
         return distances, direction
     return distances
 