d89b97f5efd027ea2fe07f091e9fd991bae3fdca
Bart Olsthoorn <bartolsthoorn@gmail.com>
2018-11-15 11:21:49 +0100

    Add Organic Materials Database (OMDB-GAP1) dataset

--- src/schnetpack/datasets/__init__.py
+++ src/schnetpack/datasets/__init__.py
@@ -6,6 +6,7 @@ Classes wrapping various standard benchmark datasets.
 from .ani1 import *
 from .iso17 import *
 from .matproj import *
+from .omdb import *
 from .md17 import *
 from .qm9 import *
 
