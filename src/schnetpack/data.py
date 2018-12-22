aa5b63e37d8ddcf31fcd3585781d1d34bc0654d6
Bart Olsthoorn <bartolsthoorn@gmail.com>
2018-12-22 17:24:21 +0100

    Allow for dataset with split file only

--- src/schnetpack/data.py
+++ src/schnetpack/data.py
@@ -67,8 +67,6 @@ class BaseAtomsData(Dataset):
             schnetpack.data.AtomsData: test dataset
 
         """
-        assert num_train + num_val <= len(
-            self), 'Dataset is smaller than num_train + num_val!'
         if split_file is not None and os.path.exists(split_file):
             S = np.load(split_file)
             train_idx = S['train_idx'].tolist()
@@ -79,6 +77,9 @@ class BaseAtomsData(Dataset):
                 raise ValueError(
                     'You have to supply either split sizes (num_train /' +
                     ' num_val) or an npz file with splits.')
+
+            assert num_train + num_val <= len(
+                self), 'Dataset is smaller than num_train + num_val!'
 
             num_train = num_train if num_train > 1 else num_train * len(self)
             num_val = num_val if num_val > 1 else num_val * len(self)
