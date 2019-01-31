2685c38f9a5f4b521fc5dc67b8c1a217fa8bd65d
Michael Gastegger <michael.gastegger@tu-berlin.de>
2019-01-31 20:28:01 +0100

    Moved model loading here and added info messages

--- src/schnetpack/sacred/calculator_ingredients.py
+++ src/schnetpack/sacred/calculator_ingredients.py
@@ -1,8 +1,9 @@
 from sacred import Ingredient
+import os
+import torch
 
 from schnetpack.md.calculators import SchnetPackCalculator
 from schnetpack.md.utils import MDUnits
-
 
 calculator_ingradient = Ingredient('calculator')
 
@@ -11,17 +12,29 @@ calculator_ingradient = Ingredient('calculator')
 def config():
     """configuration for the calculator ingredient"""
     calculator = 'schnet_calculator'
-    required_properties = ['energy', 'forces']
-    force_handle = 'forces'
+    required_properties = ['y', 'dydx']
+    force_handle = 'dydx'
     position_conversion = 1.0 / MDUnits.angs2bohr
     force_conversion = 1.0 / MDUnits.auforces2aseforces
     property_conversion = {}
 
+    model_path = 'eth_ens_01.model'
+    # If model is a directory, search for best_model file
+    if os.path.isdir(model_path):
+        model_path = os.path.join(model_path, 'best_model')
+
 
 @calculator_ingradient.capture
-def build_calculator(model, required_properties, force_handle,
+def load_model(_log, model_path, device):
+    _log.info('Loaded model from {:s}'.format(model_path))
+    model = torch.load(model_path).to(device)
+    return model
+
+
+@calculator_ingradient.capture
+def build_calculator(_log, required_properties, force_handle,
                      position_conversion, force_conversion,
-                     property_conversion, calculator):
+                     property_conversion, calculator, device):
     """
     Build the calculator object from the provided settings.
 
@@ -37,8 +50,12 @@ def build_calculator(model, required_properties, force_handle,
     Returns:
         the calculator object
     """
+    _log.info(f'Using {calculator}')
+
     if calculator == 'schnet_calculator':
-        return SchnetPackCalculator(model=model,
+
+        model = load_model(device=device)
+        return SchnetPackCalculator(model,
                                     required_properties=required_properties,
                                     force_handle=force_handle,
                                     position_conversion=position_conversion,
@@ -46,4 +63,3 @@ def build_calculator(model, required_properties, force_handle,
                                     property_conversion=property_conversion)
     else:
         raise NotImplementedError
-
