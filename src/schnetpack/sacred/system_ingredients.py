37c4310af5b105da9470d52d0a8f7e0dc437971c
Michael Gastegger <michael.gastegger@tu-berlin.de>
2019-01-31 20:35:32 +0100

    Added ring polymer config and infos

--- src/schnetpack/sacred/system_ingredients.py
+++ src/schnetpack/sacred/system_ingredients.py
@@ -3,7 +3,6 @@ from schnetpack.md.system import System
 from schnetpack.sacred.initializer_ingredient import initializer_ing, \
     build_initializer
 
-
 system_ingredient = Ingredient('system', ingredients=[initializer_ing])
 
 
@@ -14,10 +13,21 @@ def config():
     path_to_molecules = 'ethanol.xyz'
 
 
+@system_ingredient.named_config
+def ring_polymer():
+    """configuration for the system ingredient"""
+    n_replicas = 8
+    path_to_molecules = 'ethanol.xyz'
+
+
 @system_ingredient.capture
-def build_system(n_replicas, device, path_to_molecules):
+def build_system(_log, n_replicas, device, path_to_molecules):
     initializer_object = build_initializer()
+    _log.info(f'Setting up system with {n_replicas} replicas')
     system = System(n_replicas, device, initializer=initializer_object)
 
+    _log.info(f'Loading molecules from {path_to_molecules}...')
     system.load_molecules_from_xyz(path_to_molecules)
+    _log.info(f'Found {system.n_molecules} molecules...')
+
     return system
