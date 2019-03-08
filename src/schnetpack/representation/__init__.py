2f96dee7d184b8db8ee610d6743570daeb3763b9
Jan Hermann <dev@jan.hermann.name>
2019-03-08 09:46:49 +0100

    Revert "add sGDML representation"
    967fcc33d846367c3949ffa9ae9e24c3253e0c38

--- src/schnetpack/representation/__init__.py
+++ src/schnetpack/representation/__init__.py
@@ -1,26 +1,19 @@
 """
-Classes for constructing the different representations available in SchnetPack.
-This encompasses SchNet [#schnet4]_, Behler-type atom centered symmetry
-functions (ACSF) [#acsf2]_, a weighted variant thereof (wACSF) [#wacsf2]_, and
-sGDML [#sgdml].
+Classes for constructing the different representations available in SchnetPack. This encompasses SchNet [#schnet4]_,
+Behler-type atom centered symmetry functions (ACSF) [#acsf2]_ and a weighted variant thereof (wACSF) [#wacsf2]_.
 
 References
 ----------
-.. [#schnet4] Schütt, Arbabzadah, Chmiela, Müller, Tkatchenko: Quantum-chemical
-   insights from deep tensor neural networks.  Nature Communications, 8, 13890.
-   2017.
-.. [#acsf2] Behler: Atom-centered symmetry functions for constructing
-   high-dimensional neural network potentials.  The Journal of Chemical Physics
-   134. 074106. 2011.
-.. [#wacsf2] Gastegger, Schwiedrzik, Bittermann, Berzsenyi, Marquetand: wACSF --
-   Weighted atom-centered symmetry functions as descriptors in machine learning
-   potentials.  The Journal of Chemical Physics 148 (24), 241709. 2018.
-.. [#sgdml] Chmiela, Sauceda, Poltavsky, Muller, Tkatchenko: sGDML: Constructing
-   accurate and data efficient molecular force fields using machine learning.
-   Computer Physics Communications, 2019. DOI: 10.1016/j.cpc.2019.02.007.
+.. [#schnet4] Schütt, Arbabzadah, Chmiela, Müller, Tkatchenko:
+   Quantum-chemical insights from deep tensor neural networks.
+   Nature Communications, 8, 13890. 2017.
+.. [#acsf2] Behler:
+   Atom-centered symmetry functions for constructing high-dimensional neural network potentials.
+   The Journal of Chemical Physics 134. 074106. 2011.
+.. [#wacsf2] Gastegger, Schwiedrzik, Bittermann, Berzsenyi, Marquetand:
+   wACSF -- Weighted atom-centered symmetry functions as descriptors in machine learning potentials.
+   The Journal of Chemical Physics 148 (24), 241709. 2018.
 """
+
 from schnetpack.representation.schnet import SchNet, SchNetInteraction
 from schnetpack.representation.hdnn import BehlerSFBlock, StandardizeSF
-from schnetpack.representation.gdml import GDML
-
-__all__ = ['SchNet', 'SchNetInteraction', 'BehlerSFBlock', 'StandardizeSF', 'GDML']
