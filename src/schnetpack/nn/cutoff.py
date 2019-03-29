6069eb1feca8ba76c99751ab21dc3fa082cff642
Farnaz Heidar-Zadeh <farnaz_chem@yahoo.com>
2019-03-29 14:53:08 +0100

    Remove cutoff funcs & update class implementations

--- src/schnetpack/nn/cutoff.py
+++ src/schnetpack/nn/cutoff.py
@@ -6,26 +6,6 @@ from torch import nn as nn
 __all__ = ["CosineCutoff", "MollifierCutoff", "HardCutoff"]
 
 
-def cosine_cutoff(distances, cutoff=5.0):
-    """
-    Compute the Behler type cosine cutoff for the distances and return it in the
-    form of an array with the same shape as the original distances.
-
-    Args:
-        distances (torch.Tensor): Interatomic distances (Nbatch x Nat x Nneigh)
-        cutoff (float): Cutoff value, all values beyond are set to 0
-
-    Returns:
-        torch.Tensor: Tensor holding values of the cutoff function
-                      (Nbatch x Nat x Nneigh)
-    """
-    # Compute values of cutoff function
-    cutoffs = 0.5 * (torch.cos(distances * np.pi / cutoff) + 1.0)
-    # Remove contributions beyond the cutoff radius
-    cutoffs *= (distances < cutoff).float()
-    return cutoffs
-
-
 class CosineCutoff(nn.Module):
     """
     Class wrapper for cosine cutoff function.
@@ -46,27 +26,11 @@ class CosineCutoff(nn.Module):
         Returns:
             torch.Tensor: Values of cutoff function.
         """
-        return cosine_cutoff(distances, cutoff=self.cutoff)
-
-
-def mollifier_cutoff(distances, cutoff=5.0, eps=1e-7):
-    """
-    Infinitely differentiable cutoff based on a mollifier function.
-    (See https://en.wikipedia.org/wiki/Mollifier)
-
-    Args:
-        distances (torch.Tensor): Interatomic distances (Nbatch x Nat x Nneigh)
-        cutoff (float): Cutoff value, all values beyond are set to 0
-
-    Returns:
-        torch.Tensor: Tensor holding values of the cutoff function
-                      (Nbatch x Nat x Nneigh)
-    """
-    mask = (distances + eps < cutoff).float()
-    exponent = 1.0 - 1.0 / (1.0 - torch.pow(distances * mask / cutoff, 2))
-    cutoffs = torch.exp(exponent)
-    cutoffs = cutoffs * mask
-    return cutoffs
+        # Compute values of cutoff function
+        cutoffs = 0.5 * (torch.cos(distances * np.pi / self.cutoff) + 1.0)
+        # Remove contributions beyond the cutoff radius
+        cutoffs *= (distances < self.cutoff).float()
+        return cutoffs
 
 
 class MollifierCutoff(nn.Module):
@@ -74,12 +38,15 @@ class MollifierCutoff(nn.Module):
     Class wrapper for mollifier cutoff function.
 
     Args:
-        cutoff (float): Cutoff radius.
+        cutoff (float, optional): Cutoff radius.
+        eps (float, optional):
+
     """
 
-    def __init__(self, cutoff=5.0):
+    def __init__(self, cutoff=5.0, eps=1.e-7):
         super(MollifierCutoff, self).__init__()
         self.register_buffer("cutoff", torch.FloatTensor([cutoff]))
+        self.register_buffer("eps", torch.FloatTensor([eps]))
 
     def forward(self, distances):
         """
@@ -89,23 +56,11 @@ class MollifierCutoff(nn.Module):
         Returns:
             torch.Tensor: Values of cutoff function.
         """
-        return mollifier_cutoff(distances, cutoff=self.cutoff)
-
-
-def hard_cutoff(distances, cutoff=5.0):
-    """
-    Hard cutoff function.
-
-    Args:
-        distances (torch.Tensor): Interatomic distances (Nbatch x Nat x Nneigh)
-        cutoff (float): Cutoff value, all values beyond are set to 0
-
-    Returns:
-        torch.Tensor: Tensor holding values of the cutoff function
-                      (Nbatch x Nat x Nneigh)
-    """
-    mask = (distances <= cutoff).float()
-    return distances * mask
+        mask = (distances + self.eps < self.cutoff).float()
+        exponent = 1.0 - 1.0 / (1.0 - torch.pow(distances * mask / self.cutoff, 2))
+        cutoffs = torch.exp(exponent)
+        cutoffs = cutoffs * mask
+        return cutoffs
 
 
 class HardCutoff(nn.Module):
@@ -128,4 +83,5 @@ class HardCutoff(nn.Module):
         Returns:
             torch.Tensor: Values of cutoff function.
         """
-        return hard_cutoff(distances, cutoff=self.cutoff)
+        mask = (distances <= self.cutoff).float()
+        return distances * mask
