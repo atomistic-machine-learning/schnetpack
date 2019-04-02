fe63555494d25a43c2201fdd71c2d8a10539117a
Farnaz Heidar-Zadeh <farnaz_chem@yahoo.com>
2019-04-02 23:11:36 +0200

    Test GaussianSmearing class with centered=True

--- tests/test_smear.py
+++ tests/test_smear.py
@@ -18,6 +18,12 @@ def test_smear_gaussian_one_distance():
     assert len(params) == 2
     assert len(params[0]) == 6
     assert len(params[1]) == 6
+    # centered = True
+    smear = GaussianSmearing(n_gaussians=6, centered=True)
+    expt = -0.5 / torch.tensor([0., 1, 2, 3, 4, 5])**2
+    expt = torch.exp(expt * dist[:, :, :, None]**2)
+    assert torch.allclose(expt, smear(dist), atol=0.0, rtol=1.0e-7)
+    assert list(smear.parameters()) == []
 
 
 def test_smear_gaussian():
@@ -28,6 +34,11 @@ def test_smear_gaussian():
     expt = torch.tensor([[[[1, 2, 3, 4], [0, 1, 2, 3], [0.5, 0.5, 1.5, 2.5]],
                           [[.5, 1.5, 2.5, 3.5], [.5, .5, 1.5, 2.5], [2, 1, 0, 1]]]])
     expt = torch.exp(-0.5 * expt**2)
+    assert torch.allclose(expt, smear(dist), atol=0.0, rtol=1.0e-7)
+    assert list(smear.parameters()) == []
+    # centered = True
+    smear = GaussianSmearing(start=1., stop=4., n_gaussians=4, centered=True)
+    expt = torch.exp((-0.5 / torch.tensor([1, 2, 3, 4.])**2) * dist[:, :, :, None]**2)
     assert torch.allclose(expt, smear(dist), atol=0.0, rtol=1.0e-7)
     assert list(smear.parameters()) == []
 
@@ -42,6 +53,16 @@ def test_smear_gaussian_trainable():
                           [[0.5, 1.25, 2., 2.75, 3.5], [0.5, 0.25, 1., 1.75, 2.5],
                            [2., 1.25, 0.5, 0.25, 1.], [0, 0.75, 1.5, 2.25, 3.]]]])
     expt = torch.exp((-0.5 / 0.75**2) * expt**2)
+    assert torch.allclose(expt, smear(dist), atol=0.0, rtol=1.0e-7)
+    params = list(smear.parameters())
+    assert len(params) == 2
+    assert len(params[0]) == 5
+    assert len(params[1]) == 5
+    # centered = True
+    smear = GaussianSmearing(start=1., stop=4., n_gaussians=5, trainable=True,
+                             centered=True)
+    expt = -0.5 / torch.tensor([1, 1.75, 2.5, 3.25, 4])**2
+    expt = torch.exp(expt * dist[:, :, :, None]**2)
     assert torch.allclose(expt, smear(dist), atol=0.0, rtol=1.0e-7)
     params = list(smear.parameters())
     assert len(params) == 2
