9f5dbe71726087850bc187358ac22071636e07c2
Farnaz Heidar-Zadeh <farnaz_chem@yahoo.com>
2019-04-03 01:57:43 +0200

    Update CFConv class docstrings & add comments

--- src/schnetpack/nn/cfconv.py
+++ src/schnetpack/nn/cfconv.py
@@ -4,24 +4,24 @@ from torch import nn as nn
 from schnetpack.nn import Dense
 from schnetpack.nn.base import Aggregate
 
+
 __all__ = ["CFConv"]
 
 
 class CFConv(nn.Module):
-    """
-    Continuous-filter convolution layer for SchNet.
+    r"""Continuous-filter convolution block used in SchNet module.
 
     Args:
-        n_in (int): Number of input dimensions
-        n_filters (int): Number of filter dimensions
-        n_out (int): Number of output dimensions
-        filter_network (nn.Module): Calculates filter
-        cutoff_network (nn.Module): Calculates optional cutoff function
-                                    (default: None)
-        activation (function): Activation function
-        normalize_filter (bool): If true, normalize filter to number of
-                                 neighbors (default: false)
-        axis (int): axis over which convolution should be applied
+        n_in (int): number of input (i.e. atomic embedding) dimensions.
+        n_filters (int): number of filter dimensions.
+        n_out (int): number of output dimensions.
+        filter_network (nn.Module): filter block.
+        cutoff_network (nn.Module, optional): if None, no cut off function is used.
+        activation (callable, optional): if None, no activation function is used.
+        normalize_filter (bool, optional): If True, normalize filter to the number
+            of neighbors when aggregating.
+        axis (int, optional): axis over which convolution should be applied.
+
     """
 
     def __init__(
@@ -36,50 +36,50 @@ class CFConv(nn.Module):
         axis=2,
     ):
         super(CFConv, self).__init__()
-        self.in2f = Dense(n_in, n_filters, bias=False)
-        self.f2out = Dense(n_filters, n_out, activation=activation)
+        self.in2f = Dense(n_in, n_filters, bias=False, activation=None)
+        self.f2out = Dense(n_filters, n_out, bias=True, activation=activation)
         self.filter_network = filter_network
         self.cutoff_network = cutoff_network
         self.agg = Aggregate(axis=axis, mean=normalize_filter)
 
     def forward(self, x, r_ij, neighbors, pairwise_mask, f_ij=None):
-        """
+        """Compute convolution block.
+
         Args:
-            x (torch.Tensor): Input representation of atomic environments.
-            r_ij (torch.Tensor): Interatomic distances.
-            neighbors (torch.Tensor): Indices of neighboring atoms.
-            pairwise_mask (torch.Tensor): Mask to filter out non-existing
-                neighbors introduced via padding.
-            f_ij (torch.Tensor): Interatomic distances with distance expansion.
+            x (torch.Tensor): input representation/embedding of atomic environments
+                with (N_b, N_a, n_in) shape.
+            r_ij (torch.Tensor): interatomic distances of (N_b, N_a, N_nbh) shape.
+            neighbors (torch.Tensor): indices of neighbors of (N_b, N_a, N_nbh) shape.
+            pairwise_mask (torch.Tensor): mask to filter out non-existing neighbors
+                introduced via padding.
+            f_ij (torch.Tensor, optional): expanded interatomic distances in a basis.
+                If None, r_ij.unsqueeze(-1) is used.
 
         Returns:
-            torch.Tensor: Continuous convolution.
+            torch.Tensor: block output with (N_b, N_a, n_out) shape.
 
         """
         if f_ij is None:
             f_ij = r_ij.unsqueeze(-1)
 
-        # calculate filter
+        # pass expanded interactomic distances through filter block
         W = self.filter_network(f_ij)
-
-        # apply optional cutoff
+        # apply cutoff
         if self.cutoff_network is not None:
             C = self.cutoff_network(r_ij)
             W = W * C.unsqueeze(-1)
 
-        # convolution
+        # pass initial embeddings through Dense layer
         y = self.in2f(x)
-
+        # reshape y for element-wise multiplication by W
         nbh_size = neighbors.size()
         nbh = neighbors.view(-1, nbh_size[1] * nbh_size[2], 1)
         nbh = nbh.expand(-1, -1, y.size(2))
-
         y = torch.gather(y, 1, nbh)
         y = y.view(nbh_size[0], nbh_size[1], nbh_size[2], -1)
 
+        # element-wise multiplication, aggregating and Dense layer
         y = y * W
         y = self.agg(y, pairwise_mask)
-
         y = self.f2out(y)
-
         return y
