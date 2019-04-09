626440a839e9491260ee7843c3636abd864fc1c3
stefaanhess <hessmann.stefaan@googlemail.com>
2019-04-09 17:58:28 +0200

    fix scheduler state dict

---
+++ src/scripts/script_utils/evaluation.py
@@ -0,0 +1,13 @@
+def evaluate_dataset(metrics, model, loader, device):
+    for metric in metrics:
+        metric.reset()
+
+    for batch in loader:
+        batch = {k: v.to(device) for k, v in batch.items()}
+        result = model(batch)
+
+        for metric in metrics:
+            metric.add_batch(batch, result)
+
+    results = [metric.aggregate() for metric in metrics]
+    return results
