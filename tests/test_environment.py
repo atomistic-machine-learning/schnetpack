1053ebae305de0a78fac48805531b6c2d48af2eb
Dominik Lemm <lemm.d@web.de>
2019-04-24 13:46:23 +0200

    Added the TorchEnvironmentProvider to the tests

--- tests/test_environment.py
+++ tests/test_environment.py
@@ -2,6 +2,7 @@ import numpy as np
 import pytest
 from ase import Atoms
 from ase.neighborlist import neighbor_list
+import torch
 
 import schnetpack.environment as env
 
@@ -44,46 +45,67 @@ def ase_env():
     return env.AseEnvironmentProvider(10.0)
 
 
-def test_single_atom(single_atom, simple_env, ase_env):
+@pytest.fixture
+def torch_env():
+    # Select torch.device('cuda') to test on GPU
+    return env.TorchEnvironmentProvider(10.0, device=torch.device('cpu'))
+
+
+def test_single_atom(single_atom, simple_env, ase_env, torch_env):
     nbh_simple, offsets_simple = simple_env.get_environment(single_atom)
     nbh_ase, offsets_ase = ase_env.get_environment(single_atom)
+    nbh_torch, offsets_torch = torch_env.get_environment(single_atom)
 
-    assert nbh_simple.shape == nbh_ase.shape
-    assert offsets_simple.shape == offsets_ase.shape
+    assert nbh_simple.shape == nbh_ase.shape == nbh_torch.shape
+    assert offsets_simple.shape == offsets_ase.shape == offsets_torch.shape
     assert np.allclose(nbh_simple, nbh_ase)
+    assert np.allclose(nbh_simple, nbh_torch)
     assert np.allclose(offsets_simple, offsets_ase)
+    assert np.allclose(offsets_simple, offsets_torch)
 
 
-def test_two_atoms(two_atoms, simple_env, ase_env):
+def test_two_atoms(two_atoms, simple_env, ase_env, torch_env):
     nbh_simple, offsets_simple = simple_env.get_environment(two_atoms)
     nbh_ase, offsets_ase = ase_env.get_environment(two_atoms)
+    nbh_torch, offsets_torch = torch_env.get_environment(two_atoms)
 
-    assert nbh_simple.shape == nbh_ase.shape
-    assert offsets_simple.shape == offsets_ase.shape
+    assert nbh_simple.shape == nbh_ase.shape == nbh_torch.shape
+    assert offsets_simple.shape == offsets_ase.shape == offsets_torch.shape
     assert np.allclose(nbh_simple, nbh_ase)
+    assert np.allclose(nbh_simple, nbh_torch)
     assert np.allclose(offsets_simple, offsets_ase)
+    assert np.allclose(offsets_simple, offsets_torch)
 
 
-def test_single_site_crystal_small_cutoff(crystal, simple_env, ase_env):
+def test_single_site_crystal_small_cutoff(crystal, simple_env, ase_env, torch_env):
     # assure that neighboring cells are not included
-    ase_env.cutoff = 0.5
+    cutoff = 0.5
+    ase_env.cutoff = cutoff
+    torch_env.cutoff = cutoff
 
     nbh_simple, offsets_simple = simple_env.get_environment(crystal)
     nbh_ase, offsets_ase = ase_env.get_environment(crystal)
+    nbh_torch, offsets_torch = torch_env.get_environment(crystal)
 
-    assert nbh_simple.shape == nbh_ase.shape
-    assert offsets_simple.shape == offsets_ase.shape
+    assert nbh_simple.shape == nbh_ase.shape == nbh_torch.shape
+    assert offsets_simple.shape == offsets_ase.shape == offsets_torch.shape
     assert np.allclose(nbh_simple, nbh_ase)
+    assert np.allclose(nbh_simple, nbh_torch)
     assert np.allclose(offsets_simple, offsets_ase)
+    assert np.allclose(offsets_simple, offsets_torch)
 
 
-def test_single_site_crystal_large_cutoff(crystal, ase_env):
-    ase_env.cutoff = 0.7
+def test_single_site_crystal_large_cutoff(crystal, ase_env, torch_env):
+    cutoff = 0.7
+    ase_env.cutoff = cutoff
+    torch_env.cutoff = cutoff
+
     idx_i, idx_j, idx_S, dist = neighbor_list(
         "ijSd", crystal, ase_env.cutoff, self_interaction=False
     )
 
     nbh_ase, offsets_ase = ase_env.get_environment(crystal)
+    nbh_torch, offsets_torch = torch_env.get_environment(crystal)
 
     # get number of neighbors from index vector
     n_nbh = (
@@ -95,7 +117,9 @@ def test_single_site_crystal_large_cutoff(crystal, ase_env):
     )
 
     # get number of neighbors from nbh matrix
-    n_nbh_env = np.sum(nbh_ase >= 0, 1)
+    n_nbh_ase_env = np.sum(nbh_ase >= 0, 1)
+    n_nbh_torch_env = np.sum(nbh_torch >= 0, 1)
 
-    assert n_nbh.shape == n_nbh_env.shape
-    assert np.allclose(n_nbh, n_nbh_env)
+    assert n_nbh.shape == n_nbh_ase_env.shape == n_nbh_torch_env.shape
+    assert np.allclose(n_nbh, n_nbh_ase_env)
+    assert np.allclose(n_nbh, n_nbh_torch_env)
