fc330287650036a8a19fc9f972ead70e92a38ab2
stefaanhess <hessmann.stefaan@googlemail.com>
2019-05-09 15:57:40 +0200

    clean up ethanol script

--- src/examples/ethanol_script.py
+++ src/examples/ethanol_script.py
@@ -1,45 +1,31 @@
 import os
 import logging
-from shutil import rmtree
 from torch.optim import Adam
-from schnetpack.atomistic import AtomisticModel
-from schnetpack.output_modules import Atomwise
-from schnetpack.data import AtomsData, AtomsLoader, train_test_split
-from schnetpack.representation import SchNet
-from schnetpack.train import Trainer, TensorboardHook, CSVHook, ReduceLROnPlateauHook
+import schnetpack as spk
+from schnetpack.train import Trainer, CSVHook, ReduceLROnPlateauHook
 from schnetpack.metrics import MeanAbsoluteError
-from schnetpack.utils import loss_fn
+from schnetpack.metrics import mse_loss
 
 
 logging.basicConfig(level=os.environ.get("LOGLEVEL", "INFO"))
 
 
 # basic settings
-db_path = "data/md17/ethanol.db"  # relative path to the database file
 model_dir = "ethanol_model"  # directory that will be created for storing model
-properties = ["energy", "forces"]  # properties used for training
-num_train, num_val = 1000, 100  # number of training and validating samples
-batch_size = 64  # batch size used in training
-device = "cpu"  # device used, choose between 'cpu' & 'gpu'
-
-# create folders
-logging.info("datapath: {}".format(db_path))
-if os.path.exists(model_dir):
-    rmtree(model_dir)
 os.makedirs(model_dir)
+properties = ["energy", "forces"]  # properties used for training
 
 # data preparation
 logging.info("get dataset")
-dataset = AtomsData(db_path, required_properties=properties)
-train, val, test = train_test_split(
+dataset = spk.AtomsData("data/ethanol.db", required_properties=properties)
+train, val, test = spk.train_test_split(
     data=dataset,
-    num_train=num_train,
-    num_val=num_val,
+    num_train=1000,
+    num_val=100,
     split_file=os.path.join(model_dir, "split.npz"),
 )
-train_loader = AtomsLoader(train, batch_size=batch_size)
-val_loader = AtomsLoader(val, batch_size=batch_size)
-test_loader = AtomsLoader(test, batch_size=batch_size)
+train_loader = spk.AtomsLoader(train, batch_size=64)
+val_loader = spk.AtomsLoader(val, batch_size=64)
 
 # get statistics
 atomrefs = dataset.get_atomrefs(properties)
@@ -50,9 +36,9 @@ means, stddevs = train_loader.get_statistics(
 
 # model build
 logging.info("build model")
-representation = SchNet(n_interactions=6)
+representation = spk.SchNet(n_interactions=6)
 output_modules = [
-    Atomwise(
+    spk.Atomwise(
         property="energy",
         derivative="forces",
         mean=means["energy"],
@@ -60,30 +46,30 @@ output_modules = [
         negative_dr=True,
     )
 ]
-model = AtomisticModel(representation, output_modules)
+model = spk.AtomisticModel(representation, output_modules)
+
+# build optimizer
+optimizer = Adam(params=model.parameters(), lr=1e-4)
 
 # hooks
 logging.info("build trainer")
 metrics = [MeanAbsoluteError(p, p) for p in properties]
-logging_hooks = [
-    TensorboardHook(log_path=model_dir, metrics=metrics),
+hooks = [
     CSVHook(log_path=model_dir, metrics=metrics),
-]
-scheduling_hooks = [ReduceLROnPlateauHook(patience=25, window_length=3, factor=0.8)]
-hooks = logging_hooks + scheduling_hooks
+    ReduceLROnPlateauHook(optimizer)]
 
 # trainer
-loss = loss_fn(properties)
+loss = mse_loss(properties)
 trainer = Trainer(
     model_dir,
     model=model,
     hooks=hooks,
     loss_fn=loss,
-    optimizer=Adam(params=model.parameters(), lr=1e-4),
+    optimizer=optimizer,
     train_loader=train_loader,
     validation_loader=val_loader,
 )
 
 # run training
 logging.info("training")
-trainer.train(device=device, n_epochs=10000)
+trainer.train(device="cpu")
