3debcb37308e53bf4c0fcc1aa454a055e549b022
OMalenfantThuot <malenfantthuotolivier@gmail.com>
2019-06-04 13:12:24 -0400

    temp commit - not working

--- src/scripts/MOD_schnetpack_qm9.py
+++ src/scripts/MOD_schnetpack_qm9.py
@@ -37,120 +37,124 @@ if __name__ == "__main__":
         choices=dict(property=QM9.available_properties),
     )
     args = parser.parse_args()
+
+    for arg in args:
+        logging.info(arg)
     train_args = setup_run(args)
 
-    # set device
-    device = torch.device("cuda" if args.cuda else "cpu")
 
-    # define metrics
-    metrics = [
-        spk.metrics.MeanAbsoluteError(train_args.property, train_args.property),
-        spk.metrics.RootMeanSquaredError(train_args.property, train_args.property),
-    ]
-
-    # build dataset
-    logging.info("QM9 will be loaded...")
-    qm9 = QM9(
-        args.datapath,
-        download=True,
-        properties=[train_args.property],
-        collect_triples=args.model == "wacsf",
-        remove_uncharacterized=train_args.remove_uncharacterized,
-    )
-
-    # get atomrefs
-    atomref = qm9.get_atomrefs(train_args.property)
-
-    # splits the dataset in test, val, train sets
-    split_path = os.path.join(args.modelpath, "split.npz")
-    train_loader, val_loader, test_loader = get_loaders(
-        logging, args, dataset=qm9, split_path=split_path
-    )
-
-    if args.mode == "train":
-        # get statistics
-        logging.info("calculate statistics...")
-        mean, stddev = get_statistics(
-            split_path, logging, train_loader, train_args, atomref
-        )
-
-        # build representation
-        representation = get_representation(args, train_loader=train_loader)
-
-        # build output module
-        if args.model == "schnet":
-            if args.property == QM9.mu:
-                output_module = spk.output_modules.DipoleMoment(
-                    args.features,
-                    predict_magnitude=True,
-                    mean=mean[args.property],
-                    stddev=stddev[args.property],
-                    property=args.property,
-                )
-            else:
-                output_module = spk.output_modules.Atomwise(
-                    args.features,
-                    aggregation_mode=args.aggregation_mode,
-                    mean=mean[args.property],
-                    stddev=stddev[args.property],
-                    atomref=atomref[args.property],
-                    property=args.property,
-                )
-        elif args.model == "wacsf":
-            elements = frozenset((atomic_numbers[i] for i in sorted(args.elements)))
-            if args.property == QM9.mu:
-                output_module = spk.output_modules.ElementalDipoleMoment(
-                    representation.n_symfuncs,
-                    n_hidden=args.n_nodes,
-                    n_layers=args.n_layers,
-                    predict_magnitude=True,
-                    elements=elements,
-                    property=args.property,
-                )
-            else:
-                output_module = spk.output_modules.ElementalAtomwise(
-                    representation.n_symfuncs,
-                    n_hidden=args.n_nodes,
-                    n_layers=args.n_layers,
-                    aggregation_mode=args.aggregation_mode,
-                    mean=mean[args.property],
-                    stddev=stddev[args.property],
-                    atomref=atomref[args.property],
-                    elements=elements,
-                    property=args.property,
-                )
-        else:
-            raise NotImplementedError
-
-        # build AtomisticModel
-        model = get_model(
-            representation=representation,
-            output_modules=output_module,
-            parallelize=args.parallel,
-        )
-
-        # run training
-        logging.info("training...")
-        train(args, model, train_loader, val_loader, device, metrics=metrics)
-        logging.info("...training done!")
-
-    elif args.mode == "eval":
-        # load model
-        model = torch.load(os.path.join(args.modelpath, "best_model"))
-
-        # run evaluation
-        logging.info("evaluating...")
-        with torch.no_grad():
-            evaluate(
-                args,
-                model,
-                train_loader,
-                val_loader,
-                test_loader,
-                device,
-                metrics=metrics,
-                to_kcal=True,
-            )
-        logging.info("... done!")
-    else:
-        raise NotImplementedError("Unknown mode:", args.mode)
+#    # set device
+#    device = torch.device("cuda" if args.cuda else "cpu")
+#
+#    # define metrics
+#    metrics = [
+#        spk.metrics.MeanAbsoluteError(train_args.property, train_args.property),
+#        spk.metrics.RootMeanSquaredError(train_args.property, train_args.property),
+#    ]
+#
+#    # build dataset
+#    logging.info("QM9 will be loaded...")
+#    qm9 = QM9(
+#        args.datapath,
+#        download=True,
+#        properties=[train_args.property],
+#        collect_triples=args.model == "wacsf",
+#        remove_uncharacterized=train_args.remove_uncharacterized,
+#    )
+#
+#    # get atomrefs
+#    atomref = qm9.get_atomrefs(train_args.property)
+#
+#    # splits the dataset in test, val, train sets
+#    split_path = os.path.join(args.modelpath, "split.npz")
+#    train_loader, val_loader, test_loader = get_loaders(
+#        logging, args, dataset=qm9, split_path=split_path
+#    )
+#
+#    if args.mode == "train":
+#        # get statistics
+#        logging.info("calculate statistics...")
+#        mean, stddev = get_statistics(
+#            split_path, logging, train_loader, train_args, atomref
+#        )
+#
+#        # build representation
+#        representation = get_representation(args, train_loader=train_loader)
+#
+#        # build output module
+#        if args.model == "schnet":
+#            if args.property == QM9.mu:
+#                output_module = spk.output_modules.DipoleMoment(
+#                    args.features,
+#                    predict_magnitude=True,
+#                    mean=mean[args.property],
+#                    stddev=stddev[args.property],
+#                    property=args.property,
+#                )
+#            else:
+#                output_module = spk.output_modules.Atomwise(
+#                    args.features,
+#                    aggregation_mode=args.aggregation_mode,
+#                    mean=mean[args.property],
+#                    stddev=stddev[args.property],
+#                    atomref=atomref[args.property],
+#                    property=args.property,
+#                )
+#        elif args.model == "wacsf":
+#            elements = frozenset((atomic_numbers[i] for i in sorted(args.elements)))
+#            if args.property == QM9.mu:
+#                output_module = spk.output_modules.ElementalDipoleMoment(
+#                    representation.n_symfuncs,
+#                    n_hidden=args.n_nodes,
+#                    n_layers=args.n_layers,
+#                    predict_magnitude=True,
+#                    elements=elements,
+#                    property=args.property,
+#                )
+#            else:
+#                output_module = spk.output_modules.ElementalAtomwise(
+#                    representation.n_symfuncs,
+#                    n_hidden=args.n_nodes,
+#                    n_layers=args.n_layers,
+#                    aggregation_mode=args.aggregation_mode,
+#                    mean=mean[args.property],
+#                    stddev=stddev[args.property],
+#                    atomref=atomref[args.property],
+#                    elements=elements,
+#                    property=args.property,
+#                )
+#        else:
+#            raise NotImplementedError
+#
+#        # build AtomisticModel
+#        model = get_model(
+#            representation=representation,
+#            output_modules=output_module,
+#            parallelize=args.parallel,
+#        )
+#
+#        # run training
+#        logging.info("training...")
+#        train(args, model, train_loader, val_loader, device, metrics=metrics)
+#        logging.info("...training done!")
+#
+#    elif args.mode == "eval":
+#        # load model
+#        model = torch.load(os.path.join(args.modelpath, "best_model"))
+#
+#        # run evaluation
+#        logging.info("evaluating...")
+#        with torch.no_grad():
+#            evaluate(
+#                args,
+#                model,
+#                train_loader,
+#                val_loader,
+#                test_loader,
+#                device,
+#                metrics=metrics,
+#                to_kcal=True,
+#            )
+#        logging.info("... done!")
+#    else:
+#        raise NotImplementedError("Unknown mode:", args.mode)
--- src/scripts/script_utils/setup.py
+++ src/scripts/script_utils/setup.py
@@ -5,13 +5,24 @@ import schnetpack as spk
 from schnetpack.utils import to_json, read_from_json
 
 
-def setup_run(args):
+def setup_run(args, from_file="False"):
     argparse_dict = vars(args)
     jsonpath = os.path.join(args.modelpath, "args.json")
     if args.mode == "train":
         if args.overwrite and os.path.exists(args.modelpath):
             logging.info("existing model will be overwritten...")
             rmtree(args.modelpath)
+
+        if not args.overwrite and from_file:
+            if os.path.exists(jsonpath):
+                logging.info("args.json file will be read to set training.")
+                train_args = read_from_json(jsonpath)
+                spk.utils.set_random_seed(args.seed)
+                return train_args
+            else:
+                logging.info("args.json file was not found. Reset to default behavior.")
+                os.makedirs(args.modelpath)
+                to_json(jsonpath, argparse_dict)
 
         if not os.path.exists(args.modelpath):
             os.makedirs(args.modelpath)
