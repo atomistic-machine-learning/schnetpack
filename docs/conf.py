75c10eff15818c1bb324c681cfcdac736ca02e17
Kristof Schütt <kristof.schuett@tu-berlin.de>
2019-06-18 09:37:05 +0200

    Use mock imports for sphinx autodoc to circumvent issue with PyTorch 1.1

--- docs/conf.py
+++ docs/conf.py
@@ -44,6 +44,8 @@ extensions = [
     "sphinx.ext.autosectionlabel",
 ]
 
+autodoc_mock_imports = ["torch"]
+
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ["_templates"]
