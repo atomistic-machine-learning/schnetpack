4cd052af56216977adf6ca584f9e81b5423e1fdf
Michael Gastegger <michael.gastegger@tu-berlin.de>
2019-06-20 17:00:10 +0200

    Introduced updated definitions

--- src/schnetpack/data/atoms.py
+++ src/schnetpack/data/atoms.py
@@ -37,14 +37,14 @@ class AtomsData(Dataset):
     available_properties = None
 
     def __init__(
-        self,
-        dbpath,
-        subset=None,
-        required_properties=[],
-        environment_provider=SimpleEnvironmentProvider(),
-        collect_triples=False,
-        center_positions=True,
-        load_charge=False,
+            self,
+            dbpath,
+            subset=None,
+            required_properties=[],
+            environment_provider=SimpleEnvironmentProvider(),
+            collect_triples=False,
+            center_positions=True,
+            load_charge=False,
     ):
         self.dbpath = dbpath
         self.subset = subset
@@ -100,26 +100,19 @@ class AtomsData(Dataset):
         nbh_idx, offsets = self.environment_provider.get_environment(at)
 
         properties[Structure.neighbors] = torch.LongTensor(nbh_idx.astype(np.int))
-
-        properties[Structure.cell_offset] = torch.FloatTensor(
-            offsets.astype(np.float32)
-        )
+        properties[Structure.cell_offset] = torch.FloatTensor(offsets.astype(np.float32))
         properties["_idx"] = torch.LongTensor(np.array([idx], dtype=np.int))
 
         if self.collect_triples:
             nbh_idx_j, nbh_idx_k, offset_idx_j, offset_idx_k = collect_atom_triples(nbh_idx)
-            properties[Structure.neighbor_pairs_j] = torch.LongTensor(
-                nbh_idx_j.astype(np.int)
-            )
-            properties[Structure.neighbor_pairs_k] = torch.LongTensor(
-                nbh_idx_k.astype(np.int)
-            )
-            properties['offset_idx_j'] = torch.LongTensor(
-                offset_idx_j.astype(np.int)
-            )
-            properties['offset_idx_k'] = torch.LongTensor(
-                offset_idx_k.astype(np.int)
-            )
+
+            properties[Structure.neighbor_pairs_j] = torch.LongTensor(nbh_idx_j.astype(np.int))
+            properties[Structure.neighbor_pairs_k] = torch.LongTensor(nbh_idx_k.astype(np.int))
+
+            # Store offsets
+            properties[Structure.neighbor_offsets_j] = torch.LongTensor(offset_idx_j.astype(np.int))
+            properties[Structure.neighbor_offsets_k] = torch.LongTensor(offset_idx_k.astype(np.int))
+
         return properties
 
     def _subset_index(self, idx):
@@ -269,7 +262,7 @@ class AtomsData(Dataset):
 
         if len(col) == 1:
             col = col[0]
-            atomref = np.array(self.get_metadata("atomrefs"))[:, col : col + 1]
+            atomref = np.array(self.get_metadata("atomrefs"))[:, col: col + 1]
         else:
             atomref = None
 
@@ -293,15 +286,15 @@ class AtomsData(Dataset):
 
 class DownloadableAtomsData(AtomsData):
     def __init__(
-        self,
-        dbpath,
-        subset=None,
-        required_properties=None,
-        environment_provider=SimpleEnvironmentProvider(),
-        collect_triples=False,
-        center_positions=True,
-        load_charge=False,
-        download=False,
+            self,
+            dbpath,
+            subset=None,
+            required_properties=None,
+            environment_provider=SimpleEnvironmentProvider(),
+            collect_triples=False,
+            center_positions=True,
+            load_charge=False,
+            download=False,
     ):
 
         super(DownloadableAtomsData, self).__init__(
