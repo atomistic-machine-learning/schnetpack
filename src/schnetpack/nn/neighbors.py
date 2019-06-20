3f4195ca17dcbb7197e01c8c6dfaa1422e82799e
Michael Gastegger <michael.gastegger@tu-berlin.de>
2019-06-20 17:43:09 +0200

    Small restructuring

--- src/schnetpack/nn/neighbors.py
+++ src/schnetpack/nn/neighbors.py
@@ -3,13 +3,13 @@ from torch import nn
 
 
 def atom_distances(
-    positions,
-    neighbors,
-    cell=None,
-    cell_offsets=None,
-    return_vecs=False,
-    normalize_vecs=False,
-    neighbor_mask=None,
+        positions,
+        neighbors,
+        cell=None,
+        cell_offsets=None,
+        return_vecs=False,
+        normalize_vecs=False,
+        neighbor_mask=None,
 ):
     r"""Compute distance of every atom to its neighbors.
 
@@ -39,8 +39,8 @@ def atom_distances(
     # Construct auxiliary index vector
     n_batch = positions.size()[0]
     idx_m = torch.arange(n_batch, device=positions.device, dtype=torch.long)[
-        :, None, None
-    ]
+            :, None, None
+            ]
     # Get atomic positions of all neighboring indices
     pos_xyz = positions[idx_m, neighbors[:, :, :], :]
 
@@ -54,7 +54,6 @@ def atom_distances(
         offsets = cell_offsets.bmm(cell)
         offsets = offsets.view(B, A, N, D)
         dist_vec += offsets
-        
 
     # Compute vector lengths
     distances = torch.norm(dist_vec, 2, 3)
@@ -92,7 +91,7 @@ class AtomDistances(nn.Module):
         self.return_directions = return_directions
 
     def forward(
-        self, positions, neighbors, cell=None, cell_offsets=None, neighbor_mask=None
+            self, positions, neighbors, cell=None, cell_offsets=None, neighbor_mask=None
     ):
         r"""Compute distance of every atom to its neighbors.
 
@@ -123,7 +122,8 @@ class AtomDistances(nn.Module):
         )
 
 
-def triple_distances(positions, neighbors_j, neighbors_k, offset_idx_j, offset_idx_k, cell, cell_offsets):
+def triple_distances(positions, neighbors_j, neighbors_k, offset_idx_j=None, offset_idx_k=None, cell=None,
+                     cell_offsets=None):
     """
     Get all distances between atoms forming a triangle with the central atoms.
     Required e.g. for angular symmetry functions.
@@ -132,6 +132,11 @@ def triple_distances(positions, neighbors_j, neighbors_k, offset_idx_j, offset_i
         positions (torch.Tensor): Atomic positions
         neighbors_j (torch.Tensor): Indices of first neighbor in triangle
         neighbors_k (torch.Tensor): Indices of second neighbor in triangle
+        offset_idx_j (torch.Tensor): Indices for offets of neighbors j (for PBC)
+        offset_idx_k (torch.Tensor): Indices for offets of neighbors k (for PBC)
+        cell (torch.tensor, optional): periodic cell of (N_b x 3 x 3) shape.
+        cell_offsets (torch.Tensor, optional): offset of atom in cell coordinates
+            with (N_b x N_at x N_nbh x 3) shape.
 
     Returns:
         torch.Tensor: Distance between central atom and neighbor j
@@ -141,41 +146,52 @@ def triple_distances(positions, neighbors_j, neighbors_k, offset_idx_j, offset_i
     """
     nbatch, _, _ = neighbors_k.size()
     idx_m = torch.arange(nbatch, device=positions.device, dtype=torch.long)[
-        :, None, None
-    ]
+            :, None, None
+            ]
 
-    # Get the offsets into true cartesian values 
-    B, A, N, D = cell_offsets.size()
-    cell_offsets = cell_offsets.view(B, A * N, D)
-    offsets = cell_offsets.bmm(cell)
-    offsets = offsets.view(B, A, N, D)
+    pos_j = positions[idx_m, neighbors_j[:], :]
+    pos_k = positions[idx_m, neighbors_k[:], :]
 
-    # Get the offset values for j and k atoms
-    B, A, T = offset_idx_j.size()
+    if cell is not None:
+        # Get the offsets into true cartesian values
+        B, A, N, D = cell_offsets.size()
 
-    offset_idx_j = offset_idx_j.view(B*A,T)
-    offset_idx_k = offset_idx_k.view(B*A,T)
-    offsets = offsets.view(B*A,-1,3)
-    Tracer = torch.arange(B*A, device=positions.device,dtype=torch.long)[:,None]
-    offset_j = offsets[Tracer,offset_idx_j[:]].view(B,A,T,3)
-    offset_k = offsets[Tracer,offset_idx_k[:]].view(B,A,T,3)
+        cell_offsets = cell_offsets.view(B, A * N, D)
+        offsets = cell_offsets.bmm(cell)
+        offsets = offsets.view(B, A, N, D)
+
+        # Get the offset values for j and k atoms
+        B, A, T = offset_idx_j.size()
+
+        # Collapse batch and atoms position for easier indexing
+        offset_idx_j = offset_idx_j.view(B * A, T)
+        offset_idx_k = offset_idx_k.view(B * A, T)
+        offsets = offsets.view(B * A, -1, D)
+
+        # Construct auxiliary aray for advanced indexing
+        idx_offset_m = torch.arange(B * A, device=positions.device, dtype=torch.long)[:, None]
+
+        # Restore proper dmensions
+        offset_j = offsets[idx_offset_m, offset_idx_j[:]].view(B, A, T, D)
+        offset_k = offsets[idx_offset_m, offset_idx_k[:]].view(B, A, T, D)
+
+        # Add offsets
+        pos_j = pos_j + offset_j
+        pos_k = pos_k + offset_k
 
     # if positions.is_cuda:
     #    idx_m = idx_m.pin_memory().cuda(async=True)
 
     # Get the real positions of j and k
-    pos_j = positions[idx_m, neighbors_j[:], :] + offset_j
-    pos_k = positions[idx_m, neighbors_k[:], :] + offset_k
     R_ij = pos_j - positions[:, :, None, :]
     R_ik = pos_k - positions[:, :, None, :]
     R_jk = pos_j - pos_k
 
-    # + 1e-9 to avoid nan problem
-    # There could be better solutions 
-    r_ij = torch.norm(R_ij, 2, 3)+1e-9
-    r_ik = torch.norm(R_ik, 2, 3)+1e-9
-    r_jk = torch.norm(R_jk, 2, 3)+1e-9
-    
+    # + 1e-9 to avoid division by zero
+    r_ij = torch.norm(R_ij, 2, 3) + 1e-9
+    r_ik = torch.norm(R_ik, 2, 3) + 1e-9
+    r_jk = torch.norm(R_jk, 2, 3) + 1e-9
+
     return r_ij, r_ik, r_jk
 
 
@@ -222,8 +238,8 @@ def neighbor_elements(atomic_numbers, neighbors):
     n_batch = atomic_numbers.size()[0]
     # Construct auxiliary index
     idx_m = torch.arange(n_batch, device=atomic_numbers.device, dtype=torch.long)[
-        :, None, None
-    ]
+            :, None, None
+            ]
     # Get neighbors via advanced indexing
     neighbor_numbers = atomic_numbers[idx_m, neighbors[:, :, :]]
     return neighbor_numbers
