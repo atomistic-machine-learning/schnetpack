c6171146da859ed24bd49bdb39c56eb4af159bc0
Michael Gastegger <michael.gastegger@tu-berlin.de>
2019-06-24 17:26:21 +0200

    Restructuring

--- src/schnetpack/md/__init__.py
+++ src/schnetpack/md/__init__.py
@@ -1,6 +1,6 @@
 from .system import *
 from .utils import *
-from .calculators import *
+from schnetpack.md.calculators.calculators import *
 from .initial_conditions import *
 from .integrators import *
 from .neighbor_lists import *
