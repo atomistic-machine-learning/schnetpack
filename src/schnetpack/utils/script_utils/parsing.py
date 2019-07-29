5b9590851ca52825bd7648d0ed0608e81445b74b
stefaanhess <hessmann.stefaan@googlemail.com>
2019-07-29 11:10:13 +0200

    small fix

--- src/schnetpack/utils/script_utils/parsing.py
+++ src/schnetpack/utils/script_utils/parsing.py
@@ -100,6 +100,7 @@ def get_mode_parsers():
     )
     train_parser.add_argument(
         "--n_epochs",
+        type=int,
         help="Maximum number of training epochs (default: %(default)s)",
         default=1000,
     )
