68ba0d24ba258ca1b4d43fc1b7bd7e7b8f607fd5
mgastegger <michael.gastegger@tu-berlin.de>
2020-06-16 01:04:43 +0200

    Restructured hook

--- src/schnetpack/md/simulation_hooks/adaptive_sampling.py
+++ src/schnetpack/md/simulation_hooks/adaptive_sampling.py
@@ -49,24 +49,45 @@ class AdaptiveSamplingHook(SimulationHook):
         self.init_cells = copy.deepcopy(simulator.system.cells)
 
     def on_step_end(self, simulator):
+        # Check if a sample is required
+        sample_system, sample_molecule, threshold_exceeded = self._check_uncertainty(
+            simulator.system
+        )
 
+        if sample_system:
+            # Collect samples based on uncertainty thresholds
+            self._collect_samples(simulator.system, sample_molecule, threshold_exceeded)
+
+            # Reinitialize velocities if requested
+            if self.reset:
+                logging.info("Resetting system...")
+                self._reset_system(simulator.system, mask=sample_molecule)
+
+        # If a sufficient number of samples is collected, stop sampling
+        if len(self.samples) >= self.n_samples:
+            self._write_database()
+            exit()
+
+    def on_simulation_end(self, simulator):
+        # Wrap everything up if simulation finished without collecting all/any samples.
+        self._write_database()
+
+    def _check_uncertainty(self, system):
         threshold_exceeded = {}
 
         sample_system = False
         sample_molecule = torch.zeros(
-            simulator.system.n_replicas,
-            simulator.system.n_molecules,
-            device=simulator.system.device,
+            system.n_replicas, system.n_molecules, device=system.device
         ).bool()
 
         # Check if a sample is needed
         for prop in self.thresholds:
             # Get variance from simulator
-            prop_var = simulator.system.properties["{:s}_var".format(prop)]
+            prop_var = system.properties["{:s}_var".format(prop)]
 
             # Reshaping depending if property is atomic or not
             shapes = prop_var.shape
-            if shapes[2] == simulator.system.max_n_atoms:
+            if shapes[2] == system.max_n_atoms:
                 prop_var.view(*shapes[:2], -1)
                 uncertainty = torch.sqrt(torch.sum(prop_var, dim=-1))
             else:
@@ -85,63 +106,30 @@ class AdaptiveSamplingHook(SimulationHook):
                 # Overall sample required
                 sample_system = True
 
-        if sample_system:
-            # Get structures in the form of ASE atoms (R x M is flattened)
-            atoms = simulator.system.get_ase_atoms(internal_units=False)
+        return sample_system, sample_molecule, threshold_exceeded
 
-            # Collect all replicas and molecules which need sampling
-            idx_c = 0
-            for idx_r in range(simulator.system.n_replicas):
-                for idx_m in range(simulator.system.n_molecules):
+    def _collect_samples(self, system, sample_molecule, threshold_exceeded):
+        # Get structures in the form of ASE atoms (R x M is flattened)
+        atoms = system.get_ase_atoms(internal_units=False)
 
-                    # Get the atoms and store the thresholds
-                    if sample_molecule[idx_r, idx_m] == 1:
-                        self.samples.append(atoms[idx_c])
-                        sample_thresholds = {}
-                        for prop in threshold_exceeded:
-                            sample_thresholds[prop] = (
-                                threshold_exceeded[prop][idx_r, idx_m]
-                                .detach()
-                                .cpu()
-                                .numpy()
-                            )
-                        self.samples_thresholds.append(sample_thresholds)
+        # Collect all replicas and molecules which need sampling
+        idx_c = 0
+        for idx_r in range(system.n_replicas):
+            for idx_m in range(system.n_molecules):
 
-                    idx_c += 1
-
-            # TODO: Check if similar molecule is already present
-            #   -> Needs access to representation?
-
-            # Reinitialize velocities if requested
-            if self.reset:
-                logging.info("Resetting system...")
-                self._reset_system(simulator.system, mask=sample_molecule)
-
-        if len(self.samples) >= self.n_samples:
-            dataset = AtomsData(
-                self.dataset, available_properties=self.samples_thresholds[0]
-            )
-            dataset.add_systems(self.samples, self.samples_thresholds)
-            logging.info(
-                "{:d} samples written to {:s}.".format(len(self.samples), self.dataset)
-            )
-            exit()
-
-    def on_simulation_end(self, simulator):
-        # Wrap eveything up if simulation finished without collecting all/any samples.
-        if len(self.samples) > 0:
-            dataset = AtomsData(
-                self.dataset, available_properties=self.samples_thresholds[0]
-            )
-            dataset.add_systems(self.samples, self.samples_thresholds)
-            logging.info(
-                "{:d} samples written to {:s}.".format(len(self.samples), self.dataset)
-            )
-        else:
-            logging.info("No samples collected.")
-
-    def _write_sample(self, sample):
-        raise NotImplementedError
+                # Get the atoms and store the thresholds
+                if sample_molecule[idx_r, idx_m] == 1:
+                    self.samples.append(atoms[idx_c])
+                    sample_thresholds = {}
+                    for prop in threshold_exceeded:
+                        sample_thresholds[prop] = (
+                            threshold_exceeded[prop][idx_r, idx_m]
+                            .detach()
+                            .cpu()
+                            .numpy()
+                        )
+                    self.samples_thresholds.append(sample_thresholds)
+                idx_c += 1
 
     def _reset_system(self, system, mask=None):
         if mask is None:
@@ -161,3 +149,15 @@ class AdaptiveSamplingHook(SimulationHook):
                     self.init_cells[mask == 1, ...]
                 )
             self.initializer.initialize_system(system, mask=mask)
+
+    def _write_database(self):
+        if len(self.samples) > 0:
+            dataset = AtomsData(
+                self.dataset, available_properties=self.samples_thresholds[0]
+            )
+            dataset.add_systems(self.samples, self.samples_thresholds)
+            logging.info(
+                "{:d} samples written to {:s}.".format(len(self.samples), self.dataset)
+            )
+        else:
+            logging.info("No samples collected.")
