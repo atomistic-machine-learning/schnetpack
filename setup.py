2a7397aeb9ee56891686805945fcfb2389d1a9ae
stefaanhess <hessmann.stefaan@googlemail.com>
2020-07-31 12:24:24 +0200

    add __version__

--- setup.py
+++ setup.py
@@ -3,7 +3,6 @@ import io
 
 from setuptools import setup, find_packages
 
-
 def read(fname):
     with io.open(os.path.join(os.path.dirname(__file__), fname), encoding="utf-8") as f:
         return f.read()
--- src/schnetpack/__init__.py
+++ src/schnetpack/__init__.py
@@ -1,3 +1,12 @@
+try:
+    from importlib import metadata
+except ImportError:
+    # Running on pre-3.8 Python; use importlib-metadata package
+    import importlib_metadata as metadata
+
+__version__ = metadata.version("schnetpack")
+
+
 class Properties:
     """
     Keys to access structure properties in `schnetpack.data.AtomsData`
