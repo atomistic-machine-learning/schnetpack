1f673a4083479bca651e182568b7aed47158ec76
mgastegger <michael.gastegger@tu-berlin.de>
2021-03-26 15:56:05 +0100

    Added automated tests for neighbor lists

--- tests/test_data/test_transforms.py
+++ tests/test_data/test_transforms.py
@@ -10,13 +10,90 @@ def assert_consistent(orig, transformed):
         assert (v == transformed[k]).all(), f"Changed value: {k}"
 
 
-@pytest.fixture
-def aseneighbor_list(cutoff):
-    return ASENeighborList(cutoff)
+@pytest.fixture(params=[0, 1])
+def neighbor_list(request):
+    neighbor_lists = [ASENeighborList, TorchNeighborList]
+    return neighbor_lists[request.param]
 
 
-def test_single_atom(single_atom, aseneighbor_list):
-    props_after = aseneighbor_list(single_atom)
+class TestNeighborLists:
+    """
+    Test for different neighbor lists defined in neighbor_list using the Argon environment fixtures (periodic and
+    non-periodic).
+    """
+
+    def test_neighbor_list(self, neighbor_list, environment):
+        cutoff, props, neighbors_ref = environment
+        neighbor_list = neighbor_list(cutoff)
+        neighbors = neighbor_list(props)
+
+        neighbors = self._sort_neighbors(neighbors)
+        neighbors_ref = self._sort_neighbors(neighbors_ref)
+
+        for nbl, nbl_ref in zip(neighbors, neighbors_ref):
+            torch.testing.assert_allclose(nbl, nbl_ref)
+
+    def _sort_neighbors(self, neighbors):
+        """
+        Routine for sorting the index, shift and distance vectors to allow comparison between different
+        neighbor list implementations.
+
+        Args:
+            neighbors: Input dictionary holding system neighbor information (idx_i, idx_j, cell_offset and Rij)
+
+        Returns:
+            torch.LongTensor: indices of central atoms in each pair
+            torch.LongTensor: indices of each neighbor
+            torch.LongTensor: cell offsets
+            torch.Tensor: distance vectors associated with each pair
+        """
+        idx_i = neighbors[Structure.idx_i]
+        idx_j = neighbors[Structure.idx_j]
+        cell_offset = neighbors[Structure.cell_offset]
+        Rij = neighbors[Structure.Rij]
+
+        sort_idx = self._get_unique_idx(idx_i, idx_j, cell_offset)
+
+        return (
+            idx_i[sort_idx],
+            idx_j[sort_idx],
+            cell_offset[sort_idx],
+            Rij[sort_idx]
+        )
+
+    @staticmethod
+    def _get_unique_idx(idx_i: torch.Tensor, idx_j: torch.Tensor, offsets: torch.Tensor):
+        """
+        Compute unique indices for every neighbor pair based on the central atom, the neighbor and the cell the
+        neighbor belongs to. This is used for sorting the neighbor lists in order to compare between different
+        implementations.
+
+        Args:
+            idx_i: indices of central atoms in each pair
+            idx_j: indices of each neighbor
+            offsets: cell offsets
+
+        Returns:
+            torch.LongTensor: indices used for sorting each tensor in a unique manner
+        """
+        n_max = torch.max(torch.abs(offsets))
+
+        n_repeats = 2 * n_max + 1
+        n_atoms = torch.max(idx_i) + 1
+
+        unique_idx = (
+                n_repeats ** 3 * (n_atoms * idx_i + idx_j)
+                + (offsets[:, 0] + n_max)
+                + n_repeats * (offsets[:, 1] + n_max)
+                + n_repeats ** 2 * (offsets[:, 2] + n_max)
+        )
+
+        return torch.argsort(unique_idx)
+
+
+def test_single_atom(single_atom, neighbor_list, cutoff):
+    neighbor_list = neighbor_list(cutoff)
+    props_after = neighbor_list(single_atom)
     assert_consistent(single_atom, props_after)
     assert len(props_after[Structure.Rij]) == 0
     assert len(props_after[Structure.idx_i]) == 0
@@ -45,7 +122,7 @@ def test_remove_com(four_atoms):
 
     com = torch.tensor([0.0, 0.0, 0.0])
     for r_i, m_i in zip(
-        positions_trans[Structure.position], atomic_masses[four_atoms[Structure.Z]]
+            positions_trans[Structure.position], atomic_masses[four_atoms[Structure.Z]]
     ):
         com += r_i * m_i
 
