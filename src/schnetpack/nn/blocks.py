c8967d845c17322dd04cad9c1345c84e8143683e
mgastegger <michael.gastegger@tu-berlin.de>
2021-05-11 14:23:02 +0200

    Elementwise mlp and fixed list config for hidden

--- src/schnetpack/nn/blocks.py
+++ src/schnetpack/nn/blocks.py
@@ -1,9 +1,10 @@
 from typing import Union, Sequence, Callable, Optional
 
+import torch
 import torch.nn as nn
 from schnetpack.nn import Dense
 
-__all__ = ["MLP"]
+__all__ = ["MLP", "ElementwiseMLP"]
 
 
 class MLP(nn.Module):
@@ -44,6 +45,8 @@ class MLP(nn.Module):
             # get list of number of nodes hidden layers
             if type(n_hidden) is int:
                 n_hidden = [n_hidden] * (n_layers - 1)
+            else:
+                n_hidden = list(n_hidden)
             self.n_neurons = [n_in] + n_hidden + [n_out]
 
         # assign a Dense layer (with activation function) to each hidden layer
@@ -67,3 +70,51 @@ class MLP(nn.Module):
 
         """
         return self.out_net(inputs)
+
+
+class ElementwiseMLP(nn.Module):
+    def __init__(
+        self,
+        n_in: int,
+        n_out: int,
+        elements: list,
+        n_hidden: Optional[Union[int, Sequence[int]]],
+        n_layers: int = 2,
+        activation: Callable = nn.SiLU(),
+        trainable: bool = False,
+    ):
+        super(ElementwiseMLP, self).__init__()
+
+        self.n_elements = len(elements)
+        self.elements = elements
+        self.z_max = int(max(elements))
+
+        self.basic_mlps = nn.ModuleList(
+            [
+                MLP(
+                    n_in,
+                    n_out,
+                    n_hidden=n_hidden,
+                    n_layers=n_layers,
+                    activation=activation,
+                )
+                for _ in range(self.n_elements)
+            ]
+        )
+
+        elemental_weights = self._init_basis()
+        self.element_mask = nn.Embedding.from_pretrained(
+            elemental_weights, freeze=not trainable
+        )
+
+    def _init_basis(self):
+        elemental_weights = torch.zeros(self.z_max + 1, self.n_elements)
+        for idx, element in enumerate(self.elements):
+            elemental_weights[element, idx] = 1.0
+        return elemental_weights
+
+    def forward(self, atomic_numbers, representation):
+        mask = self.element_mask(atomic_numbers)
+        y = torch.cat([net(representation) for net in self.basic_mlps], dim=1)
+        y = torch.sum(y * mask, dim=1, keepdim=True)
+        return y
