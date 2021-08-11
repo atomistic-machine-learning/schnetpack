317570d822e88d379f476a029d59750a5fd14489
mgastegger <michael.gastegger@tu-berlin.de>
2021-08-11 11:20:39 +0200

    Added wrapping to box

--- src/schnetpack/md/simulation_hooks/basic_hooks.py
+++ src/schnetpack/md/simulation_hooks/basic_hooks.py
@@ -34,17 +34,23 @@ class RemoveCOMMotion(SimulationHook):
 
     Args:
         every_n_steps (int): Frequency with which motions are removed.
-        remove_rotation (bool): Also remove rotations (default=False).
+        remove_rotation (bool): Also remove rotations.
+        wrap_positions: Wrap atom positions back to box in periodic simulations.
     """
 
-    def __init__(self, every_n_steps: int = 10, remove_rotation: bool = True):
+    def __init__(self, every_n_steps: int, remove_rotation: bool, wrap_positions: bool):
         super(RemoveCOMMotion, self).__init__()
         self.every_n_steps = every_n_steps
         self.remove_rotation = remove_rotation
+        self.wrap_positions = wrap_positions
 
     def on_step_end(self, simulator: spkmd.Simulator):
         if simulator.step % self.every_n_steps == 0:
             simulator.system.remove_center_of_mass()
             simulator.system.remove_translation()
+
             if self.remove_rotation:
                 simulator.system.remove_com_rotation()
+
+            if self.wrap_positions:
+                simulator.system.wrap_positions()
