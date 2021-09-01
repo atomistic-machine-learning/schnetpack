674275af8c49e7bf1ebdbcb2ca452c8df97bc6f3
Kristof Schütt <kristof.schuett@tu-berlin.de>
2021-09-01 18:02:49 +0200

    Add long range cutoff

--- tests/test_data/conftest.py
+++ tests/test_data/conftest.py
@@ -9,12 +9,12 @@ import schnetpack as spk
 @pytest.fixture
 def single_atom():
     props = {
-        spk.properties.Z: torch.tensor(np.array([6])),
-        spk.properties.R: torch.tensor(np.array([[0.0, 0.0, 0.0]])),
+        spk.properties.Z: torch.tensor([6]),
+        spk.properties.R: torch.tensor([[0.0, 0.0, 0.0]], dtype=torch.float64),
         spk.properties.cell: torch.tensor(
-            np.array([[0.0, 0.0, 0.0], [0.0, 0.0, 0.0], [0.0, 0.0, 0.0]])
+            [[0.0, 0.0, 0.0], [0.0, 0.0, 0.0], [0.0, 0.0, 0.0]], dtype=torch.float64
         ),
-        spk.properties.pbc: torch.tensor(np.array([False, False, False])),
+        spk.properties.pbc: torch.tensor([False, False, False]),
         spk.properties.n_atoms: torch.tensor([1]),
     }
     return props
@@ -23,12 +23,14 @@ def single_atom():
 @pytest.fixture
 def two_atoms():
     props = {
-        spk.properties.Z: torch.tensor(np.array([6, 2])),
-        spk.properties.R: torch.tensor(np.array([[0.0, 0.0, 0.0], [0.1, 0.0, 0.0]])),
-        spk.properties.cell: torch.tensor(
-            np.array([[0.0, 0.0, 0.0], [0.0, 0.0, 0.0], [0.0, 0.0, 0.0]])
+        spk.properties.Z: torch.tensor([6, 2]),
+        spk.properties.R: torch.tensor(
+            [[0.0, 0.0, 0.0], [0.1, 0.0, 0.0]], dtype=torch.float64
         ),
-        spk.properties.pbc: torch.tensor(np.array([False, False, False])),
+        spk.properties.cell: torch.tensor(
+            [[0.0, 0.0, 0.0], [0.0, 0.0, 0.0], [0.0, 0.0, 0.0]], dtype=torch.float64
+        ),
+        spk.properties.pbc: torch.tensor([False, False, False]),
         spk.properties.n_atoms: torch.tensor([2]),
     }
     return props
@@ -37,16 +39,15 @@ def two_atoms():
 @pytest.fixture
 def four_atoms():
     props = {
-        spk.properties.Z: torch.tensor(np.array([6, 2, 1, 7])),
+        spk.properties.Z: torch.tensor([6, 2, 1, 7]),
         spk.properties.R: torch.tensor(
-            np.array(
-                [[0.0, 0.0, 0.0], [1.0, 0.0, 0.0], [0.0, 1.0, 0.0], [1.0, 0.0, 1.0]]
-            )
+            [[0.0, 0.0, 0.0], [1.0, 0.0, 0.0], [0.0, 1.0, 0.0], [1.0, 0.0, 1.0]],
+            dtype=torch.float64,
         ),
         spk.properties.cell: torch.tensor(
-            np.array([[0.0, 0.0, 0.0], [0.0, 0.0, 0.0], [0.0, 0.0, 0.0]])
+            [[0.0, 0.0, 0.0], [0.0, 0.0, 0.0], [0.0, 0.0, 0.0]], dtype=torch.float64
         ),
-        spk.properties.pbc: torch.tensor(np.array([False, False, False])),
+        spk.properties.pbc: torch.tensor([False, False, False]),
         spk.properties.n_atoms: torch.tensor([4]),
     }
     return props
@@ -55,10 +56,10 @@ def four_atoms():
 @pytest.fixture
 def single_site_crystal():
     props = {
-        spk.properties.Z: torch.tensor(np.array([6])),
-        spk.properties.R: torch.tensor(np.array([[0.0, 0.0, 0.0]])),
-        spk.properties.cell: torch.tensor(np.eye(3)),
-        spk.properties.pbc: torch.tensor(np.array([True, True, True])),
+        spk.properties.Z: torch.tensor([6]),
+        spk.properties.R: torch.tensor([[0.0, 0.0, 0.0]], dtype=torch.float64),
+        spk.properties.cell: torch.eye(3, dtype=torch.float64),
+        spk.properties.pbc: torch.tensor([True, True, True]),
         spk.properties.n_atoms: torch.tensor([1]),
     }
     return props
@@ -67,10 +68,12 @@ def single_site_crystal():
 @pytest.fixture
 def two_site_crystal():
     props = {
-        spk.properties.Z: torch.tensor(np.array([6, 1])),
-        spk.properties.R: torch.tensor(np.array([[0.0, 0.0, 0.0], [0.0, 0.0, 0.1]])),
-        spk.properties.cell: torch.tensor(np.eye(3)),
-        spk.properties.pbc: torch.tensor(np.array([True, True, True])),
+        spk.properties.Z: torch.tensor([6, 1]),
+        spk.properties.R: torch.tensor(
+            [[0.0, 0.0, 0.0], [0.0, 0.0, 0.1]], dtype=torch.float64
+        ),
+        spk.properties.cell: torch.eye(3, dtype=torch.float64),
+        spk.properties.pbc: torch.tensor([True, True, True]),
         spk.properties.n_atoms: torch.tensor([2]),
     }
     return props
@@ -102,20 +105,19 @@ def environment_periodic():
     props = {
         spk.properties.Z: torch.tensor(np.ones(5) * 18),
         spk.properties.R: torch.tensor(
-            np.array(
-                [
-                    [1.8475400, 3.1888300, 2.88069500],
-                    [5.1524600, 1.4996200, 4.04490500],
-                    [3.9796300, 4.4159900, 5.77417500],
-                    [4.5911200, 4.6424400, 1.67305500],
-                    [2.1663800, 1.3575600, 6.32694500],
-                ]
-            )
+            [
+                [1.8475400, 3.1888300, 2.88069500],
+                [5.1524600, 1.4996200, 4.04490500],
+                [3.9796300, 4.4159900, 5.77417500],
+                [4.5911200, 4.6424400, 1.67305500],
+                [2.1663800, 1.3575600, 6.32694500],
+            ],
+            dtype=torch.float64,
         ),
         spk.properties.cell: torch.tensor(
-            np.array([[7.0, 0.0, 0.0], [0.0, 6.0, 0.0], [0.0, 0.0, 8.0]])
+            [[7.0, 0.0, 0.0], [0.0, 6.0, 0.0], [0.0, 0.0, 8.0]], dtype=torch.float64
         ),
-        spk.properties.pbc: torch.tensor(np.array([True, True, True])),
+        spk.properties.pbc: torch.tensor([True, True, True]),
     }
     neighbors = {
         spk.properties.idx_i: torch.LongTensor(
@@ -199,46 +201,45 @@ def environment_periodic():
             ]
         ),
         spk.properties.Rij: torch.tensor(
-            np.array(
-                [
-                    [0.31884, -1.83127, -4.55375],
-                    [-3.69508, -1.68921, 1.16421],
-                    [-4.25642, 1.45361, -1.20764],
-                    [2.74358, 1.45361, -1.20764],
-                    [2.13209, 1.22716, 2.89348],
-                    [3.30492, -1.68921, 1.16421],
-                    [0.31884, -1.83127, 3.44625],
-                    [3.69508, 1.68921, -1.16421],
-                    [-2.98608, -0.14206, 2.28204],
-                    [-0.56134, 3.14282, -2.37185],
-                    [-1.17283, 2.91637, 1.72927],
-                    [-3.30492, 1.68921, -1.16421],
-                    [-1.17283, -3.08363, 1.72927],
-                    [-0.56134, -2.85718, -2.37185],
-                    [4.01392, -0.14206, 2.28204],
-                    [1.17283, 3.08363, -1.72927],
-                    [-1.81325, -3.05843, 0.55277],
-                    [-1.81325, 2.94157, 0.55277],
-                    [0.61149, 0.22645, 3.89888],
-                    [0.61149, 0.22645, -4.10112],
-                    [1.17283, -2.91637, -1.72927],
-                    [-2.13209, -1.22716, -2.89348],
-                    [-0.61149, -0.22645, -3.89888],
-                    [4.25642, -1.45361, 1.20764],
-                    [0.56134, 2.85718, 2.37185],
-                    [-0.61149, -0.22645, 4.10112],
-                    [0.56134, -3.14282, 2.37185],
-                    [-2.74358, -1.45361, 1.20764],
-                    [-2.42474, 2.71512, -3.34611],
-                    [-4.01392, 0.14206, -2.28204],
-                    [1.81325, 3.05843, -0.55277],
-                    [2.98608, 0.14206, -2.28204],
-                    [-0.31884, 1.83127, -3.44625],
-                    [2.42474, -2.71512, 3.34611],
-                    [-0.31884, 1.83127, 4.55375],
-                    [1.81325, -2.94157, -0.55277],
-                ]
-            )
+            [
+                [0.31884, -1.83127, -4.55375],
+                [-3.69508, -1.68921, 1.16421],
+                [-4.25642, 1.45361, -1.20764],
+                [2.74358, 1.45361, -1.20764],
+                [2.13209, 1.22716, 2.89348],
+                [3.30492, -1.68921, 1.16421],
+                [0.31884, -1.83127, 3.44625],
+                [3.69508, 1.68921, -1.16421],
+                [-2.98608, -0.14206, 2.28204],
+                [-0.56134, 3.14282, -2.37185],
+                [-1.17283, 2.91637, 1.72927],
+                [-3.30492, 1.68921, -1.16421],
+                [-1.17283, -3.08363, 1.72927],
+                [-0.56134, -2.85718, -2.37185],
+                [4.01392, -0.14206, 2.28204],
+                [1.17283, 3.08363, -1.72927],
+                [-1.81325, -3.05843, 0.55277],
+                [-1.81325, 2.94157, 0.55277],
+                [0.61149, 0.22645, 3.89888],
+                [0.61149, 0.22645, -4.10112],
+                [1.17283, -2.91637, -1.72927],
+                [-2.13209, -1.22716, -2.89348],
+                [-0.61149, -0.22645, -3.89888],
+                [4.25642, -1.45361, 1.20764],
+                [0.56134, 2.85718, 2.37185],
+                [-0.61149, -0.22645, 4.10112],
+                [0.56134, -3.14282, 2.37185],
+                [-2.74358, -1.45361, 1.20764],
+                [-2.42474, 2.71512, -3.34611],
+                [-4.01392, 0.14206, -2.28204],
+                [1.81325, 3.05843, -0.55277],
+                [2.98608, 0.14206, -2.28204],
+                [-0.31884, 1.83127, -3.44625],
+                [2.42474, -2.71512, 3.34611],
+                [-0.31884, 1.83127, 4.55375],
+                [1.81325, -2.94157, -0.55277],
+            ],
+            dtype=torch.float64,
         ),
     }
     return cutoff, props, neighbors
@@ -254,20 +255,19 @@ def environment_nonperiodic():
     props = {
         spk.properties.Z: torch.tensor(np.ones(5) * 18),
         spk.properties.R: torch.tensor(
-            np.array(
-                [
-                    [1.8475400, 3.1888300, 2.88069500],
-                    [5.1524600, 1.4996200, 4.04490500],
-                    [3.9796300, 4.4159900, 5.77417500],
-                    [4.5911200, 4.6424400, 1.67305500],
-                    [2.1663800, 1.3575600, 6.32694500],
-                ]
-            )
+            [
+                [1.8475400, 3.1888300, 2.88069500],
+                [5.1524600, 1.4996200, 4.04490500],
+                [3.9796300, 4.4159900, 5.77417500],
+                [4.5911200, 4.6424400, 1.67305500],
+                [2.1663800, 1.3575600, 6.32694500],
+            ],
+            dtype=torch.float64,
         ),
         spk.properties.cell: torch.tensor(
-            np.array([[7.0, 0.0, 0.0], [0.0, 6.0, 0.0], [0.0, 0.0, 8.0]])
+            [[7.0, 0.0, 0.0], [0.0, 6.0, 0.0], [0.0, 0.0, 8.0]], dtype=torch.float64
         ),
-        spk.properties.pbc: torch.tensor(np.array([False, False, False])),
+        spk.properties.pbc: torch.tensor([False, False, False]),
     }
     neighbors = {
         spk.properties.idx_i: torch.LongTensor(
@@ -277,28 +277,27 @@ def environment_nonperiodic():
             [1, 2, 3, 4, 0, 2, 3, 4, 4, 3, 1, 0, 0, 1, 2, 0, 1, 2]
         ),
         spk.properties.Rij: torch.tensor(
-            np.array(
-                [
-                    [3.30492, -1.68921, 1.16421],
-                    [2.13209, 1.22716, 2.89348],
-                    [2.74358, 1.45361, -1.20764],
-                    [0.31884, -1.83127, 3.44625],
-                    [-3.30492, 1.68921, -1.16421],
-                    [-1.17283, 2.91637, 1.72927],
-                    [-0.56134, 3.14282, -2.37185],
-                    [-2.98608, -0.14206, 2.28204],
-                    [-1.81325, -3.05843, 0.55277],
-                    [0.61149, 0.22645, -4.10112],
-                    [1.17283, -2.91637, -1.72927],
-                    [-2.13209, -1.22716, -2.89348],
-                    [-2.74358, -1.45361, 1.20764],
-                    [0.56134, -3.14282, 2.37185],
-                    [-0.61149, -0.22645, 4.10112],
-                    [-0.31884, 1.83127, -3.44625],
-                    [2.98608, 0.14206, -2.28204],
-                    [1.81325, 3.05843, -0.55277],
-                ]
-            )
+            [
+                [3.30492, -1.68921, 1.16421],
+                [2.13209, 1.22716, 2.89348],
+                [2.74358, 1.45361, -1.20764],
+                [0.31884, -1.83127, 3.44625],
+                [-3.30492, 1.68921, -1.16421],
+                [-1.17283, 2.91637, 1.72927],
+                [-0.56134, 3.14282, -2.37185],
+                [-2.98608, -0.14206, 2.28204],
+                [-1.81325, -3.05843, 0.55277],
+                [0.61149, 0.22645, -4.10112],
+                [1.17283, -2.91637, -1.72927],
+                [-2.13209, -1.22716, -2.89348],
+                [-2.74358, -1.45361, 1.20764],
+                [0.56134, -3.14282, 2.37185],
+                [-0.61149, -0.22645, 4.10112],
+                [-0.31884, 1.83127, -3.44625],
+                [2.98608, 0.14206, -2.28204],
+                [1.81325, 3.05843, -0.55277],
+            ],
+            dtype=torch.float64,
         ),
     }
     return cutoff, props, neighbors
