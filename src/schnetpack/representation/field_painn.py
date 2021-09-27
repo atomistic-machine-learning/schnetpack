Fixed tensor sign

src/schnetpack/representation/field_painn.py
@@ -62,7 +62,7 @@ class FieldInteraction(nn.Module):
             alpha_vector = self.vector_component[field](mu)
             dmu_field = (
                 alpha_scalar * external_fields[field]
-                + torch.sum(alpha_vector * external_fields[field], dim=1, keepdim=True)
+                - torch.sum(alpha_vector * external_fields[field], dim=1, keepdim=True)
                 * alpha_vector
             )
             dmu = dmu + dmu_field

src/schnetpack/representation/field_schnet.py
@@ -211,7 +211,7 @@ class DipoleInteraction(nn.Module):
 
             mu_ij = mu[field][idx_j]
             # Dipole - dipole interaction tensor
-            tensor_ij = -mu_ij * d_ij[:, None, None] ** 2 + 3.0 * v_ij[
+            tensor_ij = mu_ij * d_ij[:, None, None] ** 2 - 3.0 * v_ij[
                 :, :, None
             ] * torch.sum(v_ij[:, :, None] * mu_ij, dim=1, keepdim=True)
             tensor_ij = tensor_ij * Wij / d_ij[:, None, None] ** 5