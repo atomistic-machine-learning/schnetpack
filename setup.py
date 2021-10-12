fbf0657d44935b31197734db84cd821eef7e3072
Kristof T. Schütt <ktschuett@users.noreply.github.com>
2021-10-12 10:39:46 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -11,9 +11,9 @@ def read(fname):
 
 setup(
     name="schnetpack",
-    version="0.4.0-rc",
+    version="1.0.0",
     author="Kristof T. Schuett, Michael Gastegger, Pan Kessel, Kim Nicoli",
-    email="michael.gastegger@tu-berlin.de",
+    email="kristof.schuett@tu-berlin.de, michael.gastegger@tu-berlin.de",
     url="https://github.com/atomistic-machine-learning/schnetpack",
     packages=find_packages("src"),
     scripts=[
