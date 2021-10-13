e05f797f2bd4d297d2700cb6b970a93417c9df67
mgastegger <michael.gastegger@tu-berlin.de>
2021-10-13 14:42:21 +0200

    Added aggregation output

--- src/schnetpack/atomistic/__init__.py
+++ src/schnetpack/atomistic/__init__.py
@@ -4,3 +4,4 @@ from .longrange import *
 from .model import *
 from .nuclear_repulsion import *
 from .electrostatic import *
+from .aggregation import *
---
+++ src/schnetpack/atomistic/aggregation.py
@@ -0,0 +1,30 @@
+import torch
+import torch.nn as nn
+
+from typing import Dict, List
+
+from schnetpack import properties
+
+__all__ = ["Aggregation"]
+
+
+class Aggregation(nn.Module):
+    """
+    Aggregate predictions into a single output variable.
+
+    Args:
+        keys (list(str)): List of properties to be added.
+        output_key (str): Name of new property in output.
+    """
+
+    def __init__(self, keys: List[str], output_key: str = "y"):
+        super(Aggregation, self).__init__()
+
+        self.keys = keys
+        self.output_key = output_key
+
+    def forward(self, inputs: Dict[str, torch.Tensor]) -> Dict[str, torch.Tensor]:
+        result = {}
+        energy = torch.stack([inputs[key] for key in self.keys]).sum(0)
+        result[self.output_key] = energy
+        return result
