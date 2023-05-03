bbeb0859314e5f57aae704081f5e0ce2cd4a7936
Jonas Lederer <jonas.lederer@tu-berlin.de>
2023-05-03 17:11:32 +0200

    scalar and numpy energies possible

--- src/schnetpack/datasets/md22.py
+++ src/schnetpack/datasets/md22.py
@@ -143,7 +143,7 @@ class MD22(GDMLDataModule):
         for positions, energies, forces in zip(data["R"], data["E"], data["F"]):
             ats = Atoms(positions=positions, numbers=numbers)
             properties = {
-                self.energy: np.array([energies]),
+                self.energy: energies if type(energies) is np.ndarray else np.array([energies]),
                 self.forces: forces,
                 structure.Z: ats.numbers,
                 structure.R: ats.positions,
