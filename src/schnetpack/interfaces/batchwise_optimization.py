1bc6b6fe4a77bd0f51f8ac1f98323a254463d6d0
Jonas Lederer <jonas.lederer@tu-berlin.de>
2023-06-14 10:13:52 +0200

    improved fixed atoms mask

--- src/schnetpack/interfaces/batchwise_optimization.py
+++ src/schnetpack/interfaces/batchwise_optimization.py
@@ -187,8 +187,7 @@ class BatchwiseCalculator:
             self.calculate(atoms)
         f = self.results[self.force_key]
         if fixed_atoms_mask is not None:
-            torch.tensor(fixed_atoms_mask)
-            f = f[~torch.tensor(fixed_atoms_mask)]
+            f = f[fixed_atoms_mask]
         return f
 
     def get_potential_energy(self, atoms: List[ase.Atoms]) -> float:
@@ -411,7 +410,9 @@ class BatchwiseDynamics(Dynamics):
         self.calculator = calculator
         self.trajectory = trajectory
         self.log_every_step = log_every_step
-        self.fixed_atoms_mask = fixed_atoms_mask
+        #self.fixed_atoms_mask = fixed_atoms_mask
+        self.fixed_atoms_mask = ~torch.tensor(fixed_atoms_mask)
+
         self.n_configs = len(self.atoms)
         #self.n_atoms = len(self.atoms[0])
 
@@ -788,7 +789,7 @@ class ASEBatchwiseLBFGS(BatchwiseOptimizer):
         configs_mask = squared_max_forces < self.fmax**2
         mask = configs_mask[:, None, None].repeat(1, q_euclidean.shape[1], q_euclidean.shape[2]).view(-1, 3)
 
-        r = self.calculator.positions[~torch.tensor(self.fixed_atoms_mask)].to(torch.float64)
+        r = self.calculator.positions[self.fixed_atoms_mask].to(torch.float64)
 
         self.update(r, f, self.r0, self.f0)
 
@@ -836,7 +837,7 @@ class ASEBatchwiseLBFGS(BatchwiseOptimizer):
 
         # update positions
         pos_updated = self.calculator.positions
-        pos_updated[~torch.tensor(self.fixed_atoms_mask)] += dr
+        pos_updated[self.fixed_atoms_mask] += dr
         pos_updated = pos_updated.cpu().numpy()
 
         te = time.time()
