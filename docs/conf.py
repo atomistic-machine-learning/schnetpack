10ae5f17eefa7ebe140f2d467faf5f7cf38c13bd
Stefaanhess <hessmann.stefaan@googlemail.com>
2023-10-24 17:52:34 +0200

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -20,7 +20,7 @@
 import os
 import sys
 
-sys.path.insert(0, os.path.abspath("../src/schnetpack"))
+sys.path.insert(0, os.path.abspath("src/schnetpack"))
 
 
 # -- General configuration ------------------------------------------------
