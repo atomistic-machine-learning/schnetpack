0d340c2840ca26569a44653b44838ab3866d7984
epens94 <e.pens@tu-berlin.de>
2024-07-29 21:56:27 +0200

    only stuff for debugging remove when clean up planned

--- src/schnetpack/nn/attention.py
+++ src/schnetpack/nn/attention.py
@@ -41,6 +41,7 @@ class AttentionAggregation(nn.Module):
     
     def __init__(self,num_features):
         super().__init__()
+        self.register_parameter("help_device", torch.nn.Parameter((torch.tensor([1.]))))
         self.v_j_linear = nn.Linear(num_features,num_features,bias=False)
 
     def forward(
@@ -59,8 +60,13 @@ class AttentionAggregation(nn.Module):
 
         """
         v_j = self.v_j_linear(x)[idx_j] # shape: (n_pairs,F)
-        y = snn.scatter_add(alpha_ij[:,None] * v_j, idx_i, dim_size=x.shape[0]) # shape: (n,F)
+        #vx = torch._C._functorch.get_unwrapped(v_j)
+        v_j_1 = alpha_ij[:,None] * v_j
+        # v_j_1_wrap = torch._C._functorch.get_unwrapped(v_j_1)
+        y = snn.scatter_add(v_j_1, idx_i, dim_size=x.shape[0]) # shape: (n,F)
         return y
+        #y = snn.scatter_add(alpha_ij[:,None] * v_j, idx_i, dim_size=x.shape[0]) # shape: (n,F)
+        #return y
 
 class ConvAttentionCoefficients(nn.Module):
     
@@ -99,8 +105,9 @@ class VmapConvAttention(nn.Module):
 
     def __init__(self,nheads,num_features):
         super().__init__()
-
+        self.record = {}
         self.nheads = nheads
+        self.register_buffer("help_device", torch.LongTensor((nheads)))
         self.num_features = num_features // nheads
         self.coeff_fn = nn.ModuleList(
                 [ConvAttentionCoefficients(self.num_features) for _ in range(self.nheads)])
@@ -108,6 +115,8 @@ class VmapConvAttention(nn.Module):
         self.aggregate_fn = nn.ModuleList(
             [AttentionAggregation(self.num_features) for _ in range(self.nheads)]
             )
+
+        #self.reset_parameters()
 
     def vec_calc(self, nn, x_heads, w_heads, idx_i, idx_j):
         '''to vectorize the head calculations, resulting in speed up'''
@@ -121,19 +130,200 @@ class VmapConvAttention(nn.Module):
                 idx_i,idx_j)
         return value
 
+    def helper_params_read_in(self,data,level,device):
+
+        """this is just a helper function for debugging purposes"""
+        weights,bias = (data[level]["weights"].T,data[level]["bias"])
+
+        if len(weights.shape) == 1:
+            weights = weights[:,None]
+
+        weights = torch.nn.Parameter(torch.tensor(weights))#,device = device)
+        if bias is not None:
+            bias = torch.nn.Parameter(torch.tensor(bias))#,device = device)
+        return weights,bias
+
+    def reset_parameters(self):
+        import numpy as np
+        path = "/home/elron/phd/projects/ba_betreuung/data/params.npz"
+        data = np.load(path,allow_pickle=True)
+        feature_block = data["layers_0"].item()['FeatureBlock_0']
+        levels = list(feature_block.keys())
+        device = self.help_device.device
+
+        attention_levels = levels[4:6]
+        weights_A, bias_A = self.helper_params_read_in(feature_block,attention_levels[0],device)
+        weights_B, bias_B = self.helper_params_read_in(feature_block,attention_levels[1],device)
+
+        self.coeff_fn[0].q_i_layer.weight = torch.nn.Parameter(weights_A[:,:,0])
+        self.coeff_fn[0].k_j_layer.weight = torch.nn.Parameter(weights_B[:,:,0])
+
+        self.coeff_fn[1].q_i_layer.weight = torch.nn.Parameter(weights_A[:,:,1])
+        self.coeff_fn[1].k_j_layer.weight = torch.nn.Parameter(weights_B[:,:,1])
+
+        attention_agg_levels = levels[6:8]
+        for n,level in enumerate(attention_agg_levels):
+            weights, bias = self.helper_params_read_in(feature_block,level,device)
+            # Ansatz: first layer und second layer aggregation sieht aus wie ref wenn weights.T loaded
+            self.aggregate_fn[0].v_j_linear.weight = torch.nn.Parameter(weights[:,:,0])
+            self.aggregate_fn[1].v_j_linear.weight = torch.nn.Parameter(weights[:,:,1])
+
+    def helper(self,x):
+
+        import os
+        import numpy as np
+
+        ref_x =  np.load(
+                '/home/elron/phd/projects/ba_betreuung/data/x_feature_block_convattention.npy')
+        ref_x_heads = np.load(
+                '/home/elron/phd/projects/ba_betreuung/data/x_heads_feature_block_convattention.npy',
+                )
+        ref_w =  np.load(
+                '/home/elron/phd/projects/ba_betreuung/data/w_feature_block_convattention.npy',
+                )
+        
+        ref_w_heads = np.load(
+                '/home/elron/phd/projects/ba_betreuung/data/w_heads_feature_block_convattention.npy',
+        )
+        ref_alpha = np.load(
+                '/home/elron/phd/projects/ba_betreuung/data/alpha_feature_block_convattention.npy',
+        )
+        ref_idx_i = np.load(
+                '/home/elron/phd/projects/ba_betreuung/data/idx_i_feature_block_convattention.npy',
+        )
+        ref_idx_j = np.load(
+                '/home/elron/phd/projects/ba_betreuung/data/idx_j_feature_block_convattention.npy',
+        )
+        ref_phi_r_cut = np.load(
+                '/home/elron/phd/projects/ba_betreuung/data/phi_r_cut_feature_block_convattention.npy',
+                )
+        ref_alpha_cut = np.load(
+                '/home/elron/phd/projects/ba_betreuung/data/alpha_cut_feature_block_convattention.npy',
+                )
+        
+        ref_out = np.load(
+                '/home/elron/phd/projects/ba_betreuung/data/out_feature_block_convattention.npy',
+                )
+        ref_x_ = np.load(
+                '/home/elron/phd/projects/ba_betreuung/data/agg_alpha_feature_block_convattention.npy',
+                )
+
+        ref_v_j = np.load(
+                '/home/elron/phd/projects/ba_betreuung/data/v_j_feature_block_convattention.npy',
+                )
+        ref_v_j_1 = np.load(
+                '/home/elron/phd/projects/ba_betreuung/data/v_j_1_feature_block_convattention.npy',
+                )
+
+        ref_x = torch.tensor(ref_x,device=x.device).squeeze()
+        ref_x_heads = torch.tensor(ref_x_heads,device=x.device).squeeze()
+        ref_w = torch.tensor(ref_w,device=x.device).squeeze()
+        ref_w_heads = torch.tensor(ref_w_heads,device=x.device).squeeze()
+        ref_alpha = torch.tensor(ref_alpha,device=x.device).squeeze()
+        ref_idx_i = torch.tensor(ref_idx_i,device=x.device).squeeze()
+        ref_idx_j = torch.tensor(ref_idx_j,device=x.device).squeeze()
+        ref_phi_r_cut = torch.tensor(ref_phi_r_cut,device=x.device).squeeze()
+        ref_alpha_cut = torch.tensor(ref_alpha_cut,device=x.device).squeeze()
+        ref_out = torch.tensor(ref_out,device=x.device).squeeze()
+        ref_x_ = torch.tensor(ref_x_,device=x.device).squeeze()
+        ref_v_j = torch.tensor(ref_v_j,device=x.device).squeeze()
+        ref_v_j_1 = torch.tensor(ref_v_j_1,device=x.device).squeeze()
+
+        pairmask = torch.tensor([1., 1., 1., 1., 1., 1., 1., 1., 1., 1., 1., 1., 1., 1., 1., 1., 1.,
+        1., 1., 1., 1., 1., 1., 1., 1., 1., 1., 1., 1., 1., 1., 1., 1., 1.,
+        1., 1., 0., 0., 0., 0., 0., 0., 0., 0.], dtype=torch.float32,device=x.device)
+
+        return (ref_x,
+                ref_x_heads,
+                ref_w,
+                ref_w_heads,
+                ref_alpha,
+                ref_idx_i,
+                ref_idx_j,
+                ref_phi_r_cut,
+                ref_alpha_cut,
+                ref_out,
+                ref_x_,
+                ref_v_j,
+                ref_v_j_1,
+                pairmask)
+
+    def debug(self,x,w_ij,idx_i,idx_j,phi_r_cut):
+
+        (   ref_x,
+            ref_x_heads,
+            ref_w,
+            ref_w_heads,
+            ref_alpha,
+            ref_idx_i,
+            ref_idx_j,
+            ref_phi_r_cut,
+            ref_alpha_cut,
+            ref_out,
+            ref_x_,
+            ref_v_j,
+            ref_v_j_1,
+            pairmask) = self.helper(x)
+        ##### --------- TEST ATTENTION  #######
+        ### resultat: die vmap ist identisch zu ref so3krates
+        """
+        0. [x] selbes device wie ref benutzt (cpu cuda numerical diffs)
+        1. [x] selbe weights benutzt wie ref
+        2. [x] selbe idx_i und idx_j benutzt wie ref
+        3. [x] ref x umgeformt zu heads wie in unserer implementierung
+        4. [x] attention coeff mit umgeformten head berechnet --> test alpha
+        5. [x] ref_alpha == test_alpha
+        """
+        test_x_heads = ref_x.view(ref_x.shape[0], self.nheads, self.num_features).permute(1, 0, 2).contiguous()
+        test_w_heads = ref_w.view(ref_w.shape[0], self.nheads, self.num_features).permute(1, 0, 2).contiguous() 
+        test_alpha = self.vec_calc(self.coeff_fn, test_x_heads, test_w_heads, idx_i, idx_j) * ref_phi_r_cut[:,None]
+        #test_alpha_2 = self.vec_calc(self.coeff_fn, test_x_heads, test_w_heads, idx_i, idx_j) * ref_phi_r_cut[:,None]
+        #### ------ TEST ATTENTION AGGREGATION ####
+        """ warum ist phi_r_cut nicht gleich, checken!!!
+        1. [x] selbe weights benutzt wie ref
+        2. [x] selbe idx_i und idx_j benutzt wie ref
+        3. [x] ref alpha * v_j ist richtig
+        3. [x] test_alpha benutzt und test_x_heads benutzt
+        4. [x] geklärt was der sinn von pair mask ist
+        
+        """
+        test_alpha = test_alpha.permute(1,0).contiguous()
+
+        # nur weil beim radial mitgeschleppt wird zeros, aber wir sollten das nicht haben
+        ref_tmp_idx_i = torch.tensor([ 0,  0,  0,  1,  1,  1,  2,  2,  2,  3,  3,  3,  3,  4,  4,  5,  5,  6,
+         6,  7,  7,  8,  8,  9,  9,  9,  9, 10, 10, 10, 11, 11, 11, 12, 12, 12,
+        0, 0, 0, 0, 0, 0, 0, 0],device=x.device)
+        test_v_j_1= self.vec_calc(self.aggregate_fn, test_x_heads, test_alpha, ref_idx_i, ref_idx_j)
+        yA = self.vec_calc(self.aggregate_fn, test_x_heads, test_alpha, ref_idx_i, ref_idx_j)
+        # wenn test out alpha * v_j returned
+        # (test_v_j_1.permute(1,0,2) != ref_v_j_1).sum() ist 0 also korrekt
+        # yA == ref_out
+        # yA_test equal to ref_x_
+        yA_test = yA.contiguous().view(x.shape[0], -1)
+
+        # yB = snn.scatter_add(
+        #     test_v_j_1, 
+        #     ref_idx_i, 
+        #     dim_size=x.shape[0],
+        #     dim=0) # shape: (n,F)
+        
+        # test_x_ = yB.contiguous().view(x.shape[0], -1)
+        #test_x_ = test_out.permute(1,0,2).contiguous().view(x.shape[0], -1)
+        print("Done")
+
     def forward(self,x,w_ij,idx_i,idx_j,phi_r_cut):
 
         # first view in block structure and then permutating it
-        x_heads = x.view(x.shape[0], self.nheads, self.num_features).permute(1, 0, 2).contiguous()
+        x_heads = x.view(x.shape[0], self.nheads, self.num_features).permute(1, 0, 2).contiguous() # shape: (n_heads,n_atoms,F_head)
         w_heads = w_ij.view(w_ij.shape[0], self.nheads, self.num_features).permute(1, 0, 2).contiguous() 
 
         # calculate alpha and aggregate for every head
-        alpha = self.vec_calc(self.coeff_fn, x_heads, w_heads, idx_i, idx_j) * phi_r_cut#[:,None]
+        alpha = self.vec_calc(self.coeff_fn, x_heads, w_heads, idx_i, idx_j) * phi_r_cut  #[:,None]
         # permutation necessary for using the aggregation function
         alpha = alpha.permute(1,0).contiguous()
-        out = self.vec_calc(self.aggregate_fn, x_heads, alpha, idx_i, idx_j)
-        x_ = out.permute(1,0,2).contiguous().view(x.shape[0], -1)
-
+        self.record["alpha"] = alpha
+        y = self.vec_calc(self.aggregate_fn, x_heads, alpha, idx_i, idx_j)
+        x_ = y.contiguous().view(x.shape[0], -1)
         return x_
 
 
@@ -150,6 +340,7 @@ class VmapSphConvAttention(nn.Module):
         
         super().__init__()
         self.nheads = len(list(degrees))
+        self.register_buffer("help_device", torch.LongTensor((self.nheads)))
         self.harmonic_orders = degrees
         self.record = {}
         self.aggregate_fn = aggregate_fn
@@ -165,6 +356,84 @@ class VmapSphConvAttention(nn.Module):
             [ConvAttentionCoefficients(self.num_features) for _ in range(self.nheads)]
         )
 
+        #self.reset_parameters()
+
+    def helper_params_read_in(self,data,level,device):
+
+        """this is just a helper function for debugging purposes"""
+        weights,bias = (data[level]["weights"].T,data[level]["bias"])
+
+        if len(weights.shape) == 1:
+            weights = weights[:,None]
+
+        weights = torch.nn.Parameter(torch.tensor(weights))#,device = device)
+        if bias is not None:
+            bias = torch.nn.Parameter(torch.tensor(bias))#,device = device)
+        return weights,bias
+
+    def reset_parameters(self):
+        import numpy as np
+        path = "/home/elron/phd/projects/ba_betreuung/data/params.npz"
+        data = np.load(path,allow_pickle=True)
+        feature_block = data["layers_0"].item()['GeometricBlock_0']
+        levels = list(feature_block.keys())
+        device = self.help_device.device
+
+        attention_levels = levels[4:6]
+        weights_A, bias_A = self.helper_params_read_in(feature_block,attention_levels[0],device)
+        weights_B, bias_B = self.helper_params_read_in(feature_block,attention_levels[1],device)
+
+        self.coeff_fn[0].q_i_layer.weight = torch.nn.Parameter(weights_A[:,:])
+        self.coeff_fn[0].k_j_layer.weight = torch.nn.Parameter(weights_B[:,:])
+
+        #self.coeff_fn[1].q_i_layer.weight = torch.nn.Parameter(weights_A[:,:,1])
+        #self.coeff_fn[1].k_j_layer.weight = torch.nn.Parameter(weights_B[:,:,1])
+
+        # attention_agg_levels = levels[6:8]
+        # for n,level in enumerate(attention_agg_levels):
+        #     weights, bias = self.helper_params_read_in(feature_block,level,device)
+        #     # Ansatz: first layer und second layer aggregation sieht aus wie ref wenn weights.T loaded
+        #     self.aggregate_fn[0].v_j_linear.weight = torch.nn.Parameter(weights[:,:,0])
+        #     self.aggregate_fn[1].v_j_linear.weight = torch.nn.Parameter(weights[:,:,1])
+
+    def helper(self,x):
+
+        import os
+        import numpy as np
+
+        file_paths = {
+            'x': '/home/elron/phd/projects/ba_betreuung/data/x_geometric_block_convattention.npy',
+            'x_heads': '/home/elron/phd/projects/ba_betreuung/data/x_heads_geometric_block_convattention.npy',
+            'w': '/home/elron/phd/projects/ba_betreuung/data/w_geometric_block_convattention.npy',
+            'w_heads': '/home/elron/phd/projects/ba_betreuung/data/w_heads_geometric_block_convattention.npy',
+            
+            'alpha': '/home/elron/phd/projects/ba_betreuung/data/alpha_geometric_block_convattention.npy',
+            'alpha_r_ij': '/home/elron/phd/projects/ba_betreuung/data/alpha_r_ij_geometric_block_convattention.npy',
+            'alpha_ij_repeat': '/home/elron/phd/projects/ba_betreuung/data/alpha_ij_repeat_geometric_block_convattention.npy',
+            'alpha_ij_2': '/home/elron/phd/projects/ba_betreuung/data/alpha_ij_2_geometric_block_convattention.npy',
+            
+            'idx_i': '/home/elron/phd/projects/ba_betreuung/data/idx_i_geometric_block_convattention.npy',
+            'idx_j': '/home/elron/phd/projects/ba_betreuung/data/idx_j_geometric_block_convattention.npy',
+            
+            'phi_r_cut': '/home/elron/phd/projects/ba_betreuung/data/phi_r_cut_geometric_block_convattention.npy',
+            
+            'out': '/home/elron/phd/projects/ba_betreuung/data/out_geometric_block_convattention.npy',
+            'chi': '/home/elron/phd/projects/ba_betreuung/data/chi_geometric_block_convattention.npy',
+        }
+
+        data_dict = {}
+        for key, file_path in file_paths.items():
+            data_dict[key] = np.load(file_path)
+
+        data_dict = {key: torch.tensor(value, device=x.device).squeeze() for key, value in data_dict.items()}
+        pairmask = torch.tensor([1., 1., 1., 1., 1., 1., 1., 1., 1., 1., 1., 1., 1., 1., 1., 1., 1.,
+                         1., 1., 1., 1., 1., 1., 1., 1., 1., 1., 1., 1., 1., 1., 1., 1.,
+                         1., 1., 0., 0., 0., 0., 0., 0., 0., 0.], dtype=torch.float32, device=x.device)
+
+        data_dict['pairmask'] = pairmask
+
+        return data_dict
+
     def vec_calc(self, nn, x_heads, w_heads, idx_i, idx_j):
 
         fmodel, params, buffers = combine_state_for_ensemble(nn)
@@ -177,6 +446,42 @@ class VmapSphConvAttention(nn.Module):
                 idx_i,idx_j)
         
         return value
+
+    def debug(self,
+            chi:torch.Tensor,
+            sph_ij:torch.Tensor,
+            x: torch.Tensor,
+            w_ij: torch.Tensor,
+            idx_i: torch.Tensor,
+            phi_r_cut: torch.Tensor,
+            phi_chi_cut: torch.Tensor,
+            idx_j: torch.Tensor) -> torch.Tensor:
+        
+        ref_data = self.helper(x)
+        (ref_x, 
+            ref_x_heads, 
+            ref_w_ij, 
+            ref_w_heads, 
+            ref_alpha, 
+            ref_alpha_r_ij, 
+            ref_alpha_ij_repeat,
+            ref_alpha_ij_2, 
+            ref_idx_i, 
+            ref_idx_j, 
+            ref_phi_r_cut, 
+            ref_out, 
+            ref_chi, 
+            pairmask) = ref_data.values()
+
+        # first view in block structure and then permutating it
+        test_x_heads = ref_x.view(ref_x.shape[0], self.nheads, self.num_features).permute(1, 0, 2) # shape: (n_heads,n,F_head)
+        test_w_heads = ref_w_ij.view(ref_w_ij.shape[0], self.nheads, self.num_features).permute(1, 0, 2)  # shape (n_heads,n_pairs,F_head)
+
+        test_alpha_ij = self.vec_calc(self.coeff_fn, test_x_heads, test_w_heads, idx_i, idx_j)
+        test_alpha_r_ij = test_alpha_ij * ref_phi_r_cut[:,None] #.permute(1,0).contiguous()
+        test_alpha_ij_repeat = torch.repeat_interleave(test_alpha_r_ij,self.repeats,dim=-1) # shape: (n_pairs,m_tot)
+        test_chi_ = snn.scatter_add(test_alpha_ij_repeat * sph_ij, idx_i, dim_size=x.shape[0]) # shape: (n,m_tot)
+        ## alle werte sind identisch, one layer 
 
     def forward(
             self,
@@ -194,9 +499,10 @@ class VmapSphConvAttention(nn.Module):
         x_heads = x.view(x.shape[0], self.nheads, self.num_features).permute(1, 0, 2) # shape: (n_heads,n,F_head)
         w_heads = w_ij.view(w_ij.shape[0], self.nheads, self.num_features).permute(1, 0, 2)  # shape (n_heads,n_pairs,F_head)
 
+        # calculate alpha for every head and cut with respective cutoff fn
         alpha_ij = self.vec_calc(self.coeff_fn, x_heads, w_heads, idx_i, idx_j)
-        alpha_r_ij = (alpha_ij * phi_r_cut)#.permute(1,0).contiguous()
-        alpha_s_ij = (alpha_ij * phi_chi_cut[:,None])#.permute(1,0).contiguous()
+        alpha_r_ij = (alpha_ij * phi_r_cut) #.permute(1,0).contiguous()
+        alpha_s_ij = (alpha_ij * phi_chi_cut[:,None]) #.permute(1,0).contiguous()
 
         alpha_ij = alpha_r_ij + alpha_s_ij
         self.record["alpha"] = alpha_ij
