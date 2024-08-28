bd868966106eec6d3bac54ce0f4e33f452d2826e
stefaanhessmann <stefaan@posteo.de>
2024-08-28 16:28:00 +0200

    undo temporary fix

--- src/schnetpack/data/atoms.py
+++ src/schnetpack/data/atoms.py
@@ -359,9 +359,7 @@ class ASEAtomsData(BaseAtomsData):
             properties[structure.cell] = (
                 torch.tensor(row["cell"][None].copy()) * self.distance_conversion
             )
-            properties[structure.pbc] = torch.tensor(
-                row["pbc"].tolist()
-            )  # todo: check this RuntimeError: Could not infer dtype of numpy.bool
+            properties[structure.pbc] = torch.tensor(row["pbc"])
 
         return properties
 
