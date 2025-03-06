34728f8398e756309b75d5e810946f31dca77d68
Jonas <jonas.lederer@tu-berlin.de>
2025-03-06 19:45:38 +0100

    black

--- src/schnetpack/atomistic/atomwise.py
+++ src/schnetpack/atomistic/atomwise.py
@@ -68,7 +68,7 @@ class Atomwise(nn.Module):
 
     def forward(self, inputs: Dict[str, torch.Tensor]) -> Dict[str, torch.Tensor]:
         # predict atomwise contributions
-        y = self.outnet(inputs[" scalar_representation"])
+        y = self.outnet(inputs["scalar_representation"])
 
         # accumulate the per-atom output if necessary
         if self.per_atom_output_key is not None:
