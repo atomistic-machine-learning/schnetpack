2368e02a53ea76da9ad0ba66a3da07c1807bfd68
Jonas Lederer <50694766+jnsLs@users.noreply.github.com>
2025-03-17 14:55:14 +0100

    Jl/split mismatch error (#708)
    * raise error on split mismatch between split.npz and data.num_train, ...
    
    * Update datamodule to accept relative split size
    
    ---------
    
    Co-authored-by: sundusaijaz <sundusaijaz2011@gmail.com>

--- src/schnetpack/data/datamodule.py
+++ src/schnetpack/data/datamodule.py
@@ -255,11 +255,19 @@ class AtomsDataModule(pl.LightningDataModule):
             t.teardown()
 
     def _load_partitions(self):
-        # split dataset
+        # Split dataset
         lock = fasteners.InterProcessLock("splitting.lock")
 
         with lock:
             self._log_with_rank("Enter splitting lock")
+            # Convert relative sizes to absolute values
+            total_size = len(self.dataset)
+            if isinstance(self.num_train, float) and self.num_train <= 1:
+                self.num_train = int(self.num_train * total_size)
+            if isinstance(self.num_val, float) and self.num_val <= 1:
+                self.num_val = int(self.num_val * total_size)
+            if isinstance(self.num_test, float) and self.num_test <= 1:
+                self.num_test = int(self.num_test * total_size)
 
             if self.split_file is not None and os.path.exists(self.split_file):
                 self._log_with_rank("Load split")
@@ -268,18 +276,20 @@ class AtomsDataModule(pl.LightningDataModule):
                 self.train_idx = S["train_idx"].tolist()
                 self.val_idx = S["val_idx"].tolist()
                 self.test_idx = S["test_idx"].tolist()
+
+                # Validate if the split file matches the expected sizes
                 if self.num_train and self.num_train != len(self.train_idx):
-                    logging.warning(
+                    raise ValueError(
                         f"Split file was given, but `num_train ({self.num_train})"
                         + f" != len(train_idx)` ({len(self.train_idx)})!"
                     )
                 if self.num_val and self.num_val != len(self.val_idx):
-                    logging.warning(
+                    raise ValueError(
                         f"Split file was given, but `num_val ({self.num_val})"
                         + f" != len(val_idx)` ({len(self.val_idx)})!"
                     )
                 if self.num_test and self.num_test != len(self.test_idx):
-                    logging.warning(
+                    raise ValueError(
                         f"Split file was given, but `num_test ({self.num_test})"
                         + f" != len(test_idx)` ({len(self.test_idx)})!"
                     )
