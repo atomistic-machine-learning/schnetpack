6d897a92b593b04f4f39c3d9e2346dc25f0c0b2e
sundusaijaz <sundusaijaz2011@gmail.com>
2026-03-02 02:56:40 +0100

    refactor: update calculate_stats and estimate_atomrefs to use BaseAtomsData

--- src/schnetpack/data/stats.py
+++ src/schnetpack/data/stats.py
@@ -1,37 +1,33 @@
-from typing import Dict, Tuple
+from typing import Dict, Tuple, Optional, Any
 
 import torch
 from tqdm import tqdm
 
 import schnetpack.properties as properties
-from schnetpack.data import AtomsLoader
+from schnetpack.data.atoms import BaseAtomsData
+from schnetpack.data.loader import AtomsLoader
 
 __all__ = ["calculate_stats", "estimate_atomrefs"]
 
 
 def calculate_stats(
-    dataloader: AtomsLoader,
+    dataset: BaseAtomsData,
     divide_by_atoms: Dict[str, bool],
     atomref: Dict[str, torch.Tensor] = None,
+    batch_size: int = 10000,
+    num_workers: int = 4,
+    loader_kwargs: Optional[Dict[str, Any]] = None,
 ) -> Dict[str, Tuple[torch.Tensor, torch.Tensor]]:
-    """
-    Use the incremental Welford algorithm described in [h1]_ to accumulate
-    the mean and standard deviation over a set of samples.
+    loader_kwargs = loader_kwargs or {}
 
-    References:
-    -----------
-    .. [h1] https://en.wikipedia.org/wiki/Algorithms_for_calculating_variance
+    dataloader = AtomsLoader(
+        dataset,
+        batch_size=batch_size,
+        shuffle=True,
+        num_workers=num_workers,
+        **loader_kwargs,
+    )
 
-    Args:
-        dataloader: data loader
-        divide_by_atoms: dict from property name to bool:
-            If True, divide property by number of atoms before calculating statistics.
-        atomref: reference values for single atoms to be removed before calculating stats
-
-    Returns:
-        Mean and standard deviation over all samples
-
-    """
     property_names = list(divide_by_atoms.keys())
     norm_mask = torch.tensor(
         [float(divide_by_atoms[p]) for p in property_names], dtype=torch.float64
@@ -45,7 +41,8 @@ def calculate_stats(
         sample_values = []
         for p in property_names:
             val = props[p][None, :]
-            if atomref and p in atomref.keys():
+
+            if atomref and p in atomref:
                 ar = atomref[p]
                 ar = ar[props[properties.Z]]
                 idx_m = props[properties.idx_m]
@@ -54,90 +51,91 @@ def calculate_stats(
                 val -= v0
 
             sample_values.append(val)
+
         sample_values = torch.cat(sample_values, dim=0)
 
-        batch_size = sample_values.shape[1]
-        new_count = count + batch_size
+        batch_n = sample_values.shape[1]
+        new_count = count + batch_n
 
         norm = norm_mask[:, None] * props[properties.n_atoms][None, :] + (
             1 - norm_mask[:, None]
         )
-        sample_values /= norm
+        sample_values = sample_values / norm
 
         sample_mean = torch.mean(sample_values, dim=1)
         sample_m2 = torch.sum((sample_values - sample_mean[:, None]) ** 2, dim=1)
 
         delta = sample_mean - mean
-        mean += delta * batch_size / new_count
-        corr = batch_size * count / new_count
+        mean += delta * batch_n / new_count
+        corr = batch_n * count / new_count
         M2 += sample_m2 + delta**2 * corr
         count = new_count
 
     stddev = torch.sqrt(M2 / count)
-    stats = {pn: (mu, std) for pn, mu, std in zip(property_names, mean, stddev)}
-    return stats
+    return {pn: (mu, std) for pn, mu, std in zip(property_names, mean, stddev)}
 
 
-def estimate_atomrefs(dataloader, is_extensive, z_max=100):
-    """
-    Uses linear regression to estimate the elementwise biases (atomrefs).
+def estimate_atomrefs(
+    dataset: BaseAtomsData,
+    is_extensive: Dict[str, bool],
+    z_max: int = 100,
+    batch_size: int = 10000,
+    num_workers: int = 4,
+    loader_kwargs: Optional[Dict[str, Any]] = None,
+) -> Dict[str, torch.Tensor]:
+    loader_kwargs = loader_kwargs or {}
 
-    Args:
-        dataloader: data loader
-        is_extensive: If True, divide atom type counts by number of atoms before
-            calculating statistics.
+    dataloader = AtomsLoader(
+        dataset,
+        batch_size=batch_size,
+        shuffle=True,
+        num_workers=num_workers,
+        **loader_kwargs,
+    )
 
-    Returns:
-        Elementwise bias estimates over all samples
-
-    """
     property_names = list(is_extensive.keys())
-    n_data = len(dataloader.dataset)
+    n_data = len(dataset)
+
     all_properties = {pname: torch.zeros(n_data) for pname in property_names}
     all_atom_types = torch.zeros((n_data, z_max))
     data_counter = 0
 
-    # loop over all batches
     for batch in tqdm(dataloader, "estimating atomrefs"):
-        # load data
         idx_m = batch[properties.idx_m]
         atomic_numbers = batch[properties.Z]
 
-        # get counts for atomic numbers
-        unique_ids = torch.unique(idx_m)
-        for i in unique_ids:
+        for i in torch.unique(idx_m):
             atomic_numbers_i = atomic_numbers[idx_m == i]
             atom_types, atom_counts = torch.unique(atomic_numbers_i, return_counts=True)
-            # save atom counts and properties
+
             for atom_type, atom_count in zip(atom_types, atom_counts):
                 all_atom_types[data_counter, atom_type] = atom_count
+
             for pname in property_names:
                 property_value = batch[pname][i]
                 if not is_extensive[pname]:
                     property_value *= batch[properties.n_atoms][i]
                 all_properties[pname][data_counter] = property_value
+
             data_counter += 1
 
-    # perform linear regression to get the elementwise energy contributions
     existing_atom_types = torch.where(all_atom_types.sum(axis=0) != 0)[0]
     X = torch.squeeze(all_atom_types[:, existing_atom_types])
-    w = dict()
+
+    weights = {}
     for pname in property_names:
         if is_extensive[pname]:
-            w[pname] = torch.linalg.inv(X.T @ X) @ X.T @ all_properties[pname]
+            weights[pname] = torch.linalg.inv(X.T @ X) @ X.T @ all_properties[pname]
         else:
-            w[pname] = (
+            weights[pname] = (
                 torch.linalg.inv(X.T @ X)
                 @ X.T
                 @ (all_properties[pname] / X.sum(axis=1))
             )
 
-    # compute energy estimates
-    elementwise_contributions = {
-        pname: torch.zeros((z_max)) for pname in property_names
-    }
+    out = {pname: torch.zeros((z_max,)) for pname in property_names}
     for pname in property_names:
-        for atom_type, weight in zip(existing_atom_types, w[pname]):
-            elementwise_contributions[pname][atom_type] = weight
+        for atom_type, weight in zip(existing_atom_types, weights[pname]):
+            out[pname][atom_type] = weight
 
-    return elementwise_contributions
+    return out
