4c441bf21d2fae224fb21d2093d073db1dcebb3e
Jonas <jonas.lederer@tu-berlin.de>
2026-09-14 19:12:59 +0200

    ruff

--- tests/data/test_datasets.py
+++ tests/data/test_datasets.py
@@ -1,9 +1,10 @@
 import os
 
-from schnetpack.data import AtomsDataModule
-from schnetpack.datasets import QM9, MD17, rMD17
 import numpy as np
 import pytest
+
+from schnetpack.data import AtomsDataModule
+from schnetpack.datasets import MD17, QM9, rMD17
 
 
 @pytest.fixture
