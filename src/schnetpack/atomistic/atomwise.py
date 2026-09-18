8c3625a0a2eb4b6b0d3c0c504a704a3034f44aa1
jnsLs <jonas.lederer@mail.de>
2026-09-18 14:26:20 +0200

    style

--- src/schnetpack/atomistic/atomwise.py
+++ src/schnetpack/atomistic/atomwise.py
@@ -81,6 +81,7 @@ class Atomwise(nn.Module):
             y = snn.scatter_add(y, idx_m, dim_size=maxm)
             if self.aggregation_mode == "avg":
                 y = y / inputs[properties.n_atoms].unsqueeze(-1)
+
             y = torch.squeeze(y, -1)
 
         inputs[self.output_key] = y
